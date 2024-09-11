ea6b65beb011eacc3c40b4b06136038f4feba231
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-11 09:58:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -4,9 +4,10 @@ public:
         // XOR to find differing bits
         int xorresult=start^goal;
         int count=0;
-        while(xorresult){
-            count+=xorresult&1;
-            xorresult>>=1;
+        // extracting last digit and see it is 1 by and operation
+        while(xorresult!=0){
+           xorresult=(xorresult)&(xorresult-1);
+            count++;
         }
            return count;
     }
