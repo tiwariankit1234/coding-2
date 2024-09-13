d546f1c4c2a5e23b9db97a2ff5c42279f59cd199
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-13 10:00:00 +0530

    Time: 87 ms (20.21%), Space: 41.6 MB (90.33%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -7,21 +7,53 @@ public:
         int n=arr.size();
         int k=queries.size();
         vector<int>ans(k);
-        for(int i=0;i<k;i++){
-            int firstindex=queries[i][0];
-            int secondindex=queries[i][1];
-            int temp=0;
-            for(int j=firstindex;j<=secondindex;j++){
-              temp=temp^arr[j];
-            }
-            ans[i]=temp;
-            // if(firstelement==secondelement){
-            //     ans[i]=firstelement;
-            // }
-            // else{
-            //  ans[i]=firstelement^secondelement;
-            // }
+        vector<int>prefixxor(n);
+       
+        prefixxor[0]=arr[0];
+        
+        for(int i=1;i<n;i++){
+            prefixxor[i]=prefixxor[i-1]^arr[i];
         }
+
+      for(int i=0;i<k;i++){
+        int left=queries[i][0];
+        int right=queries[i][1];
+        if((left-1)<0){
+            ans[i]=0^prefixxor[right];
+        }
+        else{
+            ans[i]=prefixxor[left-1]^prefixxor[right];
+        }
+        
+        cout<<ans[i]<<" "<<endl;
+      }  
+          // second approach
+          //make prefixxor array and then do xor of left-1 and right
+
+
+
+
+
+
+
+
+
+
+        // for(int i=0;i<k;i++){
+        //     int firstindex=queries[i][0];
+        //     int secondindex=queries[i][1];
+        //     int temp=0;
+        //     for(int j=firstindex;j<=secondindex;j++){
+        //       temp=temp^arr[j];
+        //     }
+        //     ans[i]=temp;
+        //     // if(firstelement==secondelement){
+        //     //     ans[i]=firstelement;
+        //     // }
+        //     // else{
+        //     //  ans[i]=firstelement^secondelement;
+        //     // }
+        // }
         return ans;
     }
 };
