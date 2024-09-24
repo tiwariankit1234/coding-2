b26cda9adff57440348c59332260b29391bc29cb
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-24 11:37:49 +0530

    Time: 7 ms (64.87%), Space: 16.2 MB (38.31%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>words(wordDict.begin(),wordDict.end());
+        queue<int>q;
+        vector<bool>seen(s.length(),false);
+        int start=0;
+        q.push(start);
+        int n=s.length();
+        while(q.size()>0){
+            int start=q.front();
+            q.pop();
+            
+            if(start==s.length())return true;
+            for(int end=start+1;end<=n;end++){
 
-   
-   
- int n;
- int k[301];
- bool t(int idx,string& s,unordered_set<string>&st){
-    if(idx==n){
-        return k[idx]=true;
-    }
-     
-     if(k[idx]!=-1)
-     return k[idx];
-
-  for(int j=1;j<=n;j++){
-    string z=s.substr(idx,j);
-  
-    if(st.contains(z) and t(idx+j,s,st)){
-        return k[idx]=true;
-    }
- 
-  }
-  return k[idx]=false;
- }
-    
-    bool wordBreak(string s, vector<string>& dict) {
-      n=s.size();
-      memset(k,-1,sizeof(k));
-        unordered_set<string>st(dict.begin(),dict.end());
-        // first create set
-        // ek bool function that given string is present in the dict or not 
-        return t(0,s,st);
+                 if (seen[end]) {
+                    continue;
+                }
+                if(words.find(s.substr(start,end-start))!=words.end()){
+                    q.push(end);
+                    seen[end]=true;
+                }
+            }
+       
+        }
+        return false;
     }
 };
