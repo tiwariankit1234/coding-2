6ecf751756c1f8e7847aa7291ce47366fb900171
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-27 08:23:06 +0530

    Time: 86 ms (47.04%), Space: 41.6 MB (97.04%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,23 +1,44 @@
 class MyCalendar {
-private:
-    set<pair<int, int>> calendar;
-
 public:
-    bool book(int start, int end) {
-        const pair<int, int> event{start, end};
-        const auto nextEvent = calendar.lower_bound(event);
-        if (nextEvent != calendar.end() && nextEvent->first < end) {
-            return false;
-        }
+ vector<pair<int,int>>arr;
+ int idx=0;
 
-        if (nextEvent != calendar.begin()) {
-            const auto prevEvent = prev(nextEvent);
-            if (prevEvent->second > start) {
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        //check for each interval
+       
+       
+        //   cout<<k<<" "<<idx<<endl;
+       
+        for(int j=0;j<arr.size();j++){
+         
+            int s1=arr[j].first;
+            int e1=arr[j].second;
+            int s2=start;
+            int e2=end;
+            // cout<<s1<<" "<<e1<<" "<<s2<<" "<<e2<<" "<<endl;
+            if((e2<=s1 || e1<=s2)){
+              
+            }
+            else{
+                //  cout<<s1<<" "<<e1<<" "<<s2<<" "<<e2<<" "<<endl;
                 return false;
             }
         }
+     
+      arr.push_back({start,end});
+       return true;
 
-        calendar.insert(event);
-        return true;
+
+        
     }
 };
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
