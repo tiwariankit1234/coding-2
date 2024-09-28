cacf3bab1d279f1e699ecfd488f63ef82433ed02
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-28 15:27:54 +0530

    Time: 74 ms (67.47%), Space: 42.8 MB (27.22%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,39 +1,29 @@
 class MyCalendar {
 public:
- vector<pair<int,int>>arr;
- int idx=0;
 
+   set<pair<int,int>>s;
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        //check for each interval
-       
-       
-        //   cout<<k<<" "<<idx<<endl;
-       
-        for(int j=0;j<arr.size();j++){
+        pair<int,int>newEvent={start,end};
+
+        auto nextevent=s.lower_bound(newEvent);
+        if(nextevent!=s.end() and nextevent->first<end)
+        return false;
          
-            int s1=arr[j].first;
-            int e1=arr[j].second;
-            int s2=start;
-            int e2=end;
-            // cout<<s1<<" "<<e1<<" "<<s2<<" "<<e2<<" "<<endl;
-            if((e2<=s1 || e1<=s2)){
-              
-            }
-            else{
-                //  cout<<s1<<" "<<e1<<" "<<s2<<" "<<e2<<" "<<endl;
-                return false;
-            }
-        }
-     
-      arr.push_back({start,end});
-       return true;
+         if(nextevent!=s.begin()){
+            auto prevEvent=prev(nextevent);
+             
+            if(prevEvent->second>start)
+            return false;
 
+         }
 
-        
+        s.insert(newEvent);
+
+        return true;
     }
 };
 
