b78a6ac4a3d93c3404c7f8da62cd2a74f6ee47f7
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-28 09:42:31 +0530

    Time: 177 ms (43.92%), Space: 137.4 MB (34.43%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,53 +1,49 @@
-#include <vector>
-#include <deque>
-using namespace std;
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> ans; // To store the result
-        int n = nums.size();
+        // ek window ka nikal lunga
+       vector<int>ans;
+       int n=nums.size();
 
-        // Edge case: if nums is empty or k is 0
-        if (n == 0 || k == 0) return ans;
-
-        // Deque to store indices of elements in the current window
-        deque<int> dq;
-
-        // Process the first k elements
-        for (int i = 0; i < k; i++) {
-            // Remove indices that are out of this window
-            while (!dq.empty() && dq.front() <= i - k) {
-                dq.pop_front();
-            }
-            // Remove elements smaller than the current element from the back
-            while (!dq.empty() && nums[i] >= nums[dq.back()]) {
-                dq.pop_back(); 
-            }
-            dq.push_back(i); // Add current index to the deque
+           deque<int>dq;
+        
+       for(int i=0;i<k;i++){
+         while(!(dq.empty()) and dq.front()<=(i-k)){
+            dq.pop_front();
+         }
+        while(!dq.empty() and nums[i]>=nums[dq.back()]){
+            dq.pop_back(); 
         }
 
-        // The front of the deque is the largest element for the first window
-        ans.push_back(nums[dq.front()]);
+ 
+        dq.push_back(i);
+        // cout<<dq.front()<<" "<<endl;
+     }
+      ans.push_back(nums[dq.front()]);
+     
+       int j=k;
+       while(j<n){
 
-        // Process the remaining elements
-        for (int j = k; j < n; j++) {
-            // Remove indices that are out of this window
-            while (!dq.empty() && dq.front() <= j - k) {
-                dq.pop_front();
-            }
-
-            // Remove elements smaller than the current element from the back
-            while (!dq.empty() && nums[j] >= nums[dq.back()]) {
-                dq.pop_back(); 
-            }
-
-            dq.push_back(j); // Add current index to the deque
-            
-            // The front of the deque is the largest element for the current window
-            ans.push_back(nums[dq.front()]);
+           while(!dq.empty() and dq.front()<=(j-k)){
+            dq.pop_front();
+         }
+               
+    
+        while(!dq.empty() and nums[j]>=nums[dq.back()]){
+            dq.pop_back(); 
         }
 
-        return ans;
+        dq.push_back(j);
+       
+         
+
+      
+       
+        
+          ans.push_back(nums[dq.front()]);
+
+          j++;
+       }
+       return ans;
     }
 };
