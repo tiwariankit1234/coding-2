bd3b26c67a70a26a17f40323df4747302ebd27dd
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-28 19:22:33 +0530

    Time: 20 ms (64.1%), Space: 23.1 MB (11.65%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.cpp
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -1,95 +1,88 @@
-class MyCircularDeque {
-public:
-deque<int>dq;
-int n;
-    MyCircularDeque(int k) {
-        n=k;
-    }
-    
-    bool insertFront(int value) {
-         if(dq.size()<n){
-            dq.push_front(value);
-            return true;
-         }
-         else{
-            return false;
-         }
-    }
-    
-    bool insertLast(int value) {
-        if(dq.size()<n){
-            dq.push_back(value);
-            return true;
-         }
-         else{
-            return false;
-         }
-    }
-    
-    bool deleteFront() {
-        if(dq.size()>=1){
-            dq.pop_front();
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    
-    bool deleteLast() {
-        if(dq.size()>=1){
-            dq.pop_back();
-            return true;
-        }
-        else{
-            return false;
-        }
-        
-    }
-    
-    int getFront() {
-        if(dq.size()>=1)
-        return dq.front();
-        else{
-            return -1;
-        }
-    }
-    
-    int getRear() {
-         if(dq.size()>=1)
-        return dq.back();
-         else{
-            return -1;
-        }
-    }
-    
-    bool isEmpty() {
-          if(dq.size()>=1)
-        return false;
-        else{
-            return true;
-        }
-    }
-    
-    bool isFull() {
-        
-        if(dq.size()==n)
-        return true;
-        else{
-            return false;
-        }
-    }
+struct Node {
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val, Node* next = NULL, Node* prev = NULL)
+        : val(val), next(next), prev(prev) {}
 };
 
-/**
- * Your MyCircularDeque object will be instantiated and called as such:
- * MyCircularDeque* obj = new MyCircularDeque(k);
- * bool param_1 = obj->insertFront(value);
- * bool param_2 = obj->insertLast(value);
- * bool param_3 = obj->deleteFront();
- * bool param_4 = obj->deleteLast();
- * int param_5 = obj->getFront();
- * int param_6 = obj->getRear();
- * bool param_7 = obj->isEmpty();
- * bool param_8 = obj->isFull();
- */
+class MyCircularDeque {
+private:
+    Node* head;
+    Node* rear;
+    int size;
+    int capacity;
+
+public:
+    MyCircularDeque(int k) {
+        head = NULL;
+        rear = NULL;
+        size = 0;
+        capacity = k;
+    }
+
+    bool insertFront(int value) {
+        if (isFull()) return false;
+        if (head == NULL) {
+            head = new Node(value);
+            rear = head;
+        } else {
+            Node* newHead = new Node(value);
+            newHead->next = head;
+            head->prev = newHead;
+            head = newHead;
+        }
+        size++;
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull()) return false;
+        if (head == NULL) {
+            head = new Node(value);
+            rear = head;
+        } else {
+            Node* newNode = new Node(value, NULL, rear);
+            rear->next = newNode;
+            rear = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty()) return false;
+        if (size == 1) {
+            head = NULL;
+            rear = NULL;
+        } else {
+            Node* nextNode = head->next;
+            delete head;
+            head = nextNode;
+        }
+        size--;
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty()) return false;
+        if (size == 1) {
+            head = NULL;
+            rear = NULL;
+        } else {
+            Node* prevNode = rear->prev;
+            delete rear;
+            rear = prevNode;
+        }
+        size--;
+        return true;
+    }
+
+    int getFront() { return (isEmpty()) ? -1 : head->val; }
+
+    int getRear() { return (isEmpty()) ? -1 : rear->val; }
+
+    bool isEmpty() { return size == 0; }
+
+    bool isFull() { return size == capacity; }
+};
