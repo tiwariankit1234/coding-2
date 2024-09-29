3bfa97f24ecc30595e594c052e1fc636b9a887c6
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-09-29 15:59:23 +0530

    Time: 73 ms (70.55%), Space: 37.9 MB (89.67%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,40 +1,49 @@
 class MyCalendarTwo {
 public:
-    vector<pair<int, int>> bookings;
-    vector<pair<int, int>> overlapBookings;
-
-    MyCalendarTwo() {}
-
+ vector<pair<int,int>>singlelist,doublelist;
+    MyCalendarTwo() {
+        
+    }
+       bool check(int start1,int end1,int start,int end){
+        return (max(start,start1)<min(end1,end)); //  overlapping condition
+    }
+    
     bool book(int start, int end) {
-        // Returns false if the new booking overlaps with the existing
-        // double-booked bookings.
-        for (pair<int, int> booking : overlapBookings) {
-            if (doesOverlap(booking.first, booking.second, start, end)) {
-                return false;
-            }
-        }
-
-        // Add the double overlapping if any with the new booking.
-        for (pair<int, int> booking : bookings) {
-            if (doesOverlap(booking.first, booking.second, start, end)) {
-                overlapBookings.push_back(
-                    getOverlapped(booking.first, booking.second, start, end));
-            }
-        }
-
-        // Add the booking to the list of bookings.
-        bookings.push_back({start, end});
-        return true;
+     
+     // check with double list 
+     
+       
+            for(auto it:doublelist){
+                int start1=it.first;
+                int end1=it.second;
+                if(check(start1,end1,start,end)){
+                    return false;
+                }
     }
+    
+    // check it forms any overlap region with single list
+                for(auto it:singlelist){
+                int start1=it.first;
+                int end1=it.second;
+                if(check(start1,end1,start,end)){
+                    int x=max(start,start1);
+                    int y=min(end,end1);
+                    doublelist.push_back({x,y});
+            
+                }
+            } 
 
-private:
-    // Return true if the booking [start1, end1) & [start2, end2) overlaps.
-    bool doesOverlap(int start1, int end1, int start2, int end2) {
-        return max(start1, start2) < min(end1, end2);
-    }
+            singlelist.push_back({start,end});
+            return true;
 
-    // Return overlapping booking between [start1, end1) & [start2, end2).
-    pair<int, int> getOverlapped(int start1, int end1, int start2, int end2) {
-        return {max(start1, start2), min(end1, end2)};
-    }
+    }  
+              
+
+
 };
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
