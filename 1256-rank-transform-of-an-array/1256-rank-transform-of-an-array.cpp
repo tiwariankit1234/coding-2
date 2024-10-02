44660d5917ce82ecc25e8e8ecbe5fbd92de3e04b
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-02 11:32:32 +0530

    Time: 103 ms (26.95%), Space: 49.9 MB (18.32%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -1,42 +1,65 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        int n=arr.size();
-        vector<int>copy(arr);
+        // int n=arr.size();
+
+        // if(n==0)return {};
+
+
+        // vector<int>copy(arr);
         // sort the copy array
-        sort(copy.begin(),copy.end());
+        // sort(copy.begin(),copy.end());
 
-        int prevrank=1;
-        int prevelement=copy[0];
+        // int prevrank=1;
+        // int prevelement=copy[0];
 
-        vector<int>rank(n);
+        //  map<int,int>mp;
+        //    mp.insert({prevelement,prevrank});
 
-         map<int,int>mp;
-           mp.insert({prevelement,prevrank});
+        //    int currentrank;
 
-           int currentrank;
+        //    for(int i=1;i<n;i++){
+        //      int currentelement=copy[i];
+        //      if(currentelement==prevelement){
+        //         currentrank=prevrank;
+        //         mp.insert({currentelement,currentrank});
+        //      }
 
-           for(int i=1;i<n;i++){
-             int currentelement=copy[i];
-             if(currentelement==prevelement){
-                currentrank=prevrank;
-                mp.insert({currentelement,currentrank});
-             }
+        //      else{
+        //         currentrank=prevrank+1;
+        //         mp.insert({currentelement,currentrank});
+        //      }
 
-             else{
-                currentrank=prevrank+1;
-                mp.insert({currentelement,currentrank});
-             }
+        //      prevrank=currentrank;
+        //      prevelement=currentelement;
 
-             prevrank=currentrank;
-             prevelement=currentelement;
+        //    }
 
-           }
+        //    for(int i=0;i<n;i++){
+        //     int num=arr[i];
+        //     arr[i]=mp[num];
+        //    }
+        //    return arr;
 
-           for(int i=0;i<n;i++){
+
+        //second approach
+            
+         int n=arr.size();   
+        unordered_map<int,int>mp;
+        set<int>s(arr.begin(),arr.end());
+
+         int rank=1;
+        for(int num:s){
+            mp.insert({num,rank});
+            rank++;
+        }
+
+        for(int i=0;i<n;i++){
             int num=arr[i];
-            rank[i]=mp[num];
-           }
-           return rank;
+            arr[i]=mp[arr[i]];
+        }
+
+        return arr;
+
     }
 };
