6cf60a764a1a15dbd3d330fcb2538e2c99f29952
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-02 17:44:19 +0530

    Time: 53 ms (84%), Space: 51.2 MB (87.36%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+   let count=init;
+   function increment(){
+        return (++count);
+    }
+   function reset(){
+       return count=init;
+    }
+   function decrement(){
+        return --count;
+    }
+    
+    return {increment,reset,decrement};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
