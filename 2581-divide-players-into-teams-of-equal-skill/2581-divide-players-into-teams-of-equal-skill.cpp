7420239f706ba46e624bcaf63dd5be5a92bfe969
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-05 23:56:37 +0530

    Time: 64 ms (58.06%), Space: 56.9 MB (20.56%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-    long long dividePlayers(vector<int>& arr) {
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
+    long long dividePlayers(vector<int>& skill) {
+        int n=skill.size();
+        int totalskill=0;
+        // to store the total skill
+        for(auto i:skill)
+        totalskill+=i;
 
-        int sum=arr[0]+arr[n-1];
-          long long productskill=(arr[0]*arr[n-1]);
+
+        cout<<totalskill<<endl;
+
+        if((totalskill%(n/2))!=0)return -1;
+
+       
+
+         long long targetskill=totalskill/(n/2);
+        int requiredskill=0;
+
+        unordered_map<int,int>mp;
+        for(auto i:skill)
+         mp[i]++;
+
+         long long totalchemistry=0;
+
+         for(int i=0;i<n;i++){
+            int givenskill=skill[i];
+            requiredskill=targetskill-givenskill;
+           if(mp.find(requiredskill)==mp.end()||mp[requiredskill]!=mp[givenskill]){
            
-           int i=1,j=n-2;
-          while(i<j){
-             int newsum=arr[i]+arr[j];
-             if(newsum!=sum)return -1;
-             productskill+=arr[i]*arr[j];
+            
+            return -1;
 
-             i++;
-             j--;
-          }
-          return productskill;
+           }
+        //    else{
+        //     mp[givenskill]--;
+        //     mp[requiredskill]--;
+        //     if(mp[givenskill]==0)mp.erase(givenskill);
+        //     if(mp[requiredskill]==0)mp.erase(requiredskill);
+        //     totalchemistry+=givenskill*requiredskill;
+        //    }
+          totalchemistry+=givenskill*requiredskill;
+         }
+         return totalchemistry/2;
     }
 };
