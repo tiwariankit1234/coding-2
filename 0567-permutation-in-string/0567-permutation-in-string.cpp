94410e86c7d493142487db464ec435eb1e1f30bf
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-05 22:43:16 +0530

    Time: 5 ms (70.09%), Space: 8.7 MB (92.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,46 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int k=s2.size()-s1.size();
-        int n=s2.size();
-        // cout<<n<<endl;
-        
-        // cout<<s1.size()<<endl;
+        int n=s1.size();
+        int k=s2.size();
 
-        // unordered_map<char,int>mp1,mp2;
+         vector<int>arr1(26);
+         vector<int>arr2(26);
 
-       vector<int>arr1(26);
-    
-
-
-       for(int i=0;i<s1.size();i++){
-         int index=s1[i]-'a';
-         arr1[index]++;
-       }
-
-     
-
-          string newstring="";
-        for(int i=0;i<=k;i++){
-              
-               vector<int>arr2(26);
-               int count=0;
-            for(int j=i;j<n;j++){
-                count++;
-                if(count>(s1.size()))break;
-                 arr2[s2[j]-'a']++;
-                //   if(arr1==arr2){
-                //     // cout<<i<<" "<<j<<endl;
-                //     return true;
-
-                //   }
-                  
+         for(int i=0;i<n;i++)
+         arr1[s1[i]-'a']++;
+             
+         int i=0,j=0;
+         int count=0;
+         while(j<k){
+            arr2[s2[j]-'a']++;
+            count++;
+            if(count>n){
+                arr2[s2[i]-'a']--;
+                i++;
+                count=n;
             }
-                if(arr1==arr2){
-                    // cout<<i<<" "<<j<<endl;
-                    return true;
-        }
-    } 
-        return false;
+            if(arr1==arr2)return true;
+            j++;
+
+         }
+         return false;
     }
 };
