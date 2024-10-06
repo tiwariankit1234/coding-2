22d687e7733b4bb608673d2853ebabb7dcfacc99
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-06 18:38:48 +0530

    Time: 0 ms (100%), Space: 8.4 MB (66.36%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,80 +1,15 @@
 class Solution {
 public:
-    bool areSentencesSimilar(string s1, string s2) {
-        int m=s1.size();
-        int n=s2.size();
+    bool areSentencesSimilar(string a, string b) {
+        stringstream s(a),ss(b);
+        string str;
+        deque<string>d1,d2;
+        while(s>>str)d1.push_back(str);
+        while(ss>>str)d2.push_back(str);
 
-        deque<string>dq1,dq2;
-            
-            string s="";
-        for(int i=0;i<m;i++){
-         if(s1[i]!=' ')
-            s+=s1[i];
-         
-         else{
-            dq1.push_back(s);
-            s="";
-         }
-    }  
-    dq1.push_back(s);
-        
-        
-          s="";
-        for(int i=0;i<n;i++){
-         if(s2[i]!=' ')
-            s+=s2[i];
-         
-         else{
-            dq2.push_back(s);
-            s="";
-         }
-    }  
+        while(!d1.empty() and !d2.empty() and d1.front()==d2.front())d1.pop_front(),d2.pop_front();
+        while(!d1.empty() and !d2.empty() and d1.back()==d2.back())d1.pop_back(),d2.pop_back();
 
-    dq2.push_back(s);
-
-    // for(auto it:s1)
-    // cout<<it<<" ";
-
-    // cout<<endl;
-
-    //  for(auto it:s2)
-    // cout<<it<<" ";
-
-  
-
-          
-               cout<<dq1.size()<<" "<<dq2.size()<<endl;
-
-         // see the front
-         while(dq1.size() and dq2.size()){
-            string first=dq1.front();
-            string second=dq2.front();
-            if(first!=second)break;
-            else
-            {
-               
-                dq1.pop_front();
-                dq2.pop_front();
-            }
-         }
-             
-               cout<<dq1.size()<<" "<<dq2.size()<<endl;
-          // see the back
-            while(dq1.size() and dq2.size()){
-            string first=dq1.back();
-            string second=dq2.back();
-            if(first!=second)break;
-            else
-            {
-                dq1.pop_back();
-                dq2.pop_back();
-            }
-         }
-
-        //  cout<<dq1.size()<<" "<<dq2.size()<<endl;
-
-         return (dq1.empty() || dq2.empty());
-
-
+        return d1.empty() || d2.empty();
     }
 };
