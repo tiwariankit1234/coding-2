974f3269998adbcf485a6edc49322d87e67bb442
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-07 22:55:49 +0530

    Time: 10 ms (22.06%), Space: 9.9 MB (97.42%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int minLength(string s) {
-        while (true) {
-            int n = s.size();
-            bool found = false;  // To check if we found and erased any "AB" or "CD"
-            
-            for (int j = 0; j < n - 1; ++j) {
-                if ((s[j] == 'A' && s[j + 1] == 'B') || (s[j] == 'C' && s[j + 1] == 'D')) {
-                    s.erase(j, 2);  // Erase the pair of characters
-                    found = true;
-                    break;  // Break the loop and restart to handle the updated string
-                }
+         int n=s.size();
+         int i=0,j=1;
+
+         while(j<n){
+            if((i>=0) and ((s[i]=='A' and s[j]=='B') || (s[i]=='C' and s[j]=='D'))){
+                i--;
+             
             }
-            
-            // If no "AB" or "CD" was found, exit the loop
-            if (!found) break;
-        }
-        
-        return s.size();  // Return the final size of the string
+            else{
+                i++;
+                s[i]=s[j];
+               
+            }
+            j++;
+         }
+         return i+1;
     }
 };
