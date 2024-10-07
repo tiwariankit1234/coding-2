c3eb9952589c4bffb445c108dfdeb0d22fe8e829
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-07 21:49:45 +0530

    Time: 53 ms (45.87%), Space: 49.2 MB (10.52%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -2,9 +2,12 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = (...args) =>
-   args.length;
-
+var argumentsLength = function(...args) {
+   let num=0;
+   for(let i of args)
+   num++
+   return num;
+};
 
 /**
  * argumentsLength(1, 2, 3); // 3
