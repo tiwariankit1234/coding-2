1e6d46ac348ae8e5b4cc64e93f0b042d478df57f
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-08 17:50:01 +0530

    Time: 122 ms (31.82%), Space: 36.3 MB (12.86%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,18 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
 class Solution {
 public:
-    int minSwaps(string s) {
-        stack<char>st;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(s[i]==']'){
-                if(!st.empty()and (st.top()=='[')){
-                    st.pop();
+    int minSwaps(std::string s) {
+        std::stack<char> stk;
+        int swap = 0;
+
+        for (char ch : s) {
+            if (ch == '[') {
+                stk.push(ch);
+            } else {
+                if (!stk.empty() && stk.top() == '[') {
+                    stk.pop();
+                } else {
+                    // No match -> swap -> thus push '[' (after swapped)
+                    stk.push('[');
+                    swap++;
                 }
             }
-            else{
-                st.push(s[i]);
-            }
         }
-        return (st.size()+1)/2;
+
+        return swap;
     }
 };
