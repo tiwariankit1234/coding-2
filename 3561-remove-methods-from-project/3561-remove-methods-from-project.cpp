e8ceb5c25061d77957e80da1ee617393f9a55465
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-11 19:57:25 +0530

    Time: 897 ms (50.3%), Space: 361.9 MB (23.74%) - LeetHub

--- 3561-remove-methods-from-project/3561-remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/3561-remove-methods-from-project.cpp
@@ -1,48 +1,61 @@
 class Solution {
 public:
-    // DFS to find all methods that are suspicious (reachable from method k)
-    void dfs(int i, const std::vector<int> adj[], std::unordered_set<int>& suspicious) {
-        suspicious.insert(i); // Mark this method as suspicious
-
-        for (auto child : adj[i]) {
-            if (suspicious.find(child) == suspicious.end()) {
-                dfs(child, adj, suspicious); // Recursively mark all invoked methods
-            }
+ 
+ void dfs1(int i,vector<int>adj[],vector<int>&visited,vector<int>&ans,int n){
+    visited[i]=1;
+   
+    // cout<<"5"<<endl;
+   
+    for(auto child:adj[i]){
+        if(visited[child]==0){
+          dfs1(child,adj,visited,ans,n);
         }
-    }
+    }  
+      
+      return ;
+ }
 
-    std::vector<int> remainingMethods(int n, int k, const std::vector<std::vector<int>>& invocations) {
-        std::vector<int> adj[n]; // Adjacency list to represent the graph
-        std::unordered_set<int> suspicious; // Set to track suspicious methods
-
-        // Build the adjacency list
-        for (const auto& it : invocations) {
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<int>adj[n];
+         
+         vector<int>visited(n,0);
+        for(auto it:invocations){
             adj[it[0]].push_back(it[1]);
         }
+        vector<int>ans;
 
-        // Perform DFS starting from method k to find all suspicious methods
-        dfs(k, adj, suspicious);
+        dfs1(k,adj,visited,ans,n);
 
-        // Check if any non-suspicious method invokes a suspicious method
-        for (const auto& it : invocations) {
-            int invoker = it[0], invoked = it[1];
-            if (suspicious.find(invoked) != suspicious.end() && suspicious.find(invoker) == suspicious.end()) {
-                vector<int>ans;
+            // for(auto it:visited)
+            // cout<<it<<endl;
+
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                // cout<<"5"<<endl;
+               ans.push_back(i);
+            }
+        }
+
+        // check there is no internal edge calling suspicious node
+           
+   
+
+        for(auto it:invocations){
+
+    
+         int a=it[0];
+         
+            int b=it[1];
+            if(!visited[a] and visited[b]){
+                ans.clear();
                 for(int i=0;i<n;i++){
-                    ans.push_back(i);// Return all methods if any suspicious is invoked by a non-suspicious
-            }
-            return ans;
-        }
-        }   
-
-        // Collect remaining methods that are not in the suspicious set
-        std::vector<int> remaining;
-        for (int i = 0; i < n; ++i) {
-            if (suspicious.find(i) == suspicious.end()) {
-                remaining.push_back(i);
+                    ans.push_back(i);
+                }
+                return ans;
             }
         }
 
-        return remaining; // Return the remaining methods
+        
+        return ans;
     }
 };
