813e572bb2159c0ff39e944bbd24809f4a0895d6
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-11 21:56:42 +0530

    Time: 58 ms (73.65%), Space: 61.8 MB (27.27%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=n-1;
+        int water=0;
+
+        while(i<=j){
+            if(nums[i]<=nums[j]){
+                water=max(water,min(nums[i],nums[j])*(j-i));
+                i++;
+            }
+            else{
+                water=max(water,min(nums[i],nums[j])*(j-i));
+                j--;
+            }
+        }
+        return water;
+    }
+};
