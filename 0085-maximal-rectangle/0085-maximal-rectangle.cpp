37c1e3a99ad39fe5b92566687885cc1049c2dd41
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-29 21:02:17 +0530

    Time: 20 ms (22.99%), Space: 22.7 MB (15.15%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,42 +1,99 @@
 class Solution {
 public:
+
+void prev(vector<int>& arr,int n,vector<int>&prevsmaller){
+     stack<int>st;
+     st.push(0);
+    prevsmaller[0]=-1;     
+
+    for(int i=1;i<n;i++){
+        while(!st.empty() and arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        if(st.size()==0)prevsmaller[i]=-1;
+        else{
+            prevsmaller[i]=st.top();
+        }
+        st.push(i);
+    }
+    //   for(auto it:prevsmaller)
+    //    cout<<it<<" ";
+
+       cout<<endl;
+}
+
+void next(vector<int>& arr,int n,vector<int>&nextsmaller){
+    stack<int>st;
+    st.push(n-1);
+    nextsmaller[n-1]=n;
+
+    for(int i=n-2;i>=0;i--){
+        while(!st.empty() and arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        if(st.size()==0)nextsmaller[i]=n;
+        else{
+            nextsmaller[i]=st.top();
+        }
+        st.push(i);
+    }
+    //  cout<<"5"<<endl;
+    // for(auto it:nextsmaller)
+    //    cout<<it<<" ";
+
+       cout<<endl;
+}
+
+ 
+    int largestRectangleArea(vector<int>& heights) {
+         int n=heights.size();
+         if(n==1)return heights[0];
+         vector<int>prevsmaller(n);
+         prev(heights,n,prevsmaller);
+        
+         vector<int>nextsmaller(n);
+         next(heights,n,nextsmaller);
+          
+           int maxarea=0;
+         for(int i=0;i<n;i++){
+            int currentheight=heights[i];
+            int width=nextsmaller[i]-prevsmaller[i]-1;
+            int area=currentheight*width;
+            maxarea=max(maxarea,area);
+         }
+         return maxarea;
+    }
+
+
+
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
+        int m=matrix.size();
+        int n=matrix[0].size();
 
-        int maxarea=0;
+        vector<vector<int>>newmatrix(m,vector<int>(n,0));
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                  int minwidth=cols;
-                if(matrix[i][j]=='1'){
-                    int length=0;
-                   
-                    for(int k=i;k<rows;k++){
-                        if(matrix[k][j]=='0'){
-                        break;
-                        } 
-                           length++;
-                             
-                           int width=0;   
-                          for(int l=j;l<cols;l++){
-                            if(matrix[k][l]=='1')
-                            width++;
-                            else{
-                               break; 
-                            }
-                          }
-                          minwidth=min(width,minwidth);
-
-                         int area=minwidth*length;
-                        // cout<<area<<" "<<i<<" "<<j<<" "<<endl;
-                        maxarea=max(area,maxarea);
-                        
-
-                        
-                    }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0){
+                    newmatrix[i][j]=matrix[i][j]-'0';
+                }
+                else if(matrix[i][j]=='1'){
+                    newmatrix[i][j]+=newmatrix[i-1][j]+1;
+                    // cout<<i<<" "<<j<<endl;
                 }
             }
+        }
+
+        // for(auto it:newmatrix){
+        //     for(auto t:it){
+        //         cout<<t<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+     int maxarea=0;
+        for(auto it:newmatrix){
+       maxarea=max(maxarea,largestRectangleArea(it));
         }
         return maxarea;
     }
