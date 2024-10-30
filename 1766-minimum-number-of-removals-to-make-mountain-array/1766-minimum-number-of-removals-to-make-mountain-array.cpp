20ba2b77c132129ce2c9159bebb43d1533fee445
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-10-30 16:35:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,37 +1,77 @@
 class Solution {
 public:
+ 
+ vector<int>dp1;
+ 
+ vector<int>dp2,arr;
+
+
+ 
+ void lengthOfLIS(vector<int>& nums) {
+        arr=nums;
+        int n=nums.size();
+      dp1.resize(n,1);
+  
+    int ans=0;
+      for(int i=0;i<nums.size();i++){
+          for(int j=0;j<=i-1;j++){
+              if(arr[j]<arr[i])
+              dp1[i]=max(dp1[i],1+dp1[j]);
+          }
+   
+      ans=max(ans,dp1[i]);
+      }
+   
+    
+      return ;
+    }
+
+     void lengthOfLDS(vector<int>& nums) {
+        arr=nums;
+        int n=nums.size();
+      dp2.resize(n,1);
+  
+      int ans=0;
+      for(int i=n-2;i>=0;i--){
+        
+          for(int j=n-1;j>=i+1;j--){
+              if(arr[j]<arr[i])
+              dp2[i]=max(dp2[i],1+dp2[j]);
+          }
+   
+      ans=max(ans,dp2[i]);
+      
+      } 
+   
+    
+      return ;
+    }
+
+
+
+
+
     int minimumMountainRemovals(vector<int>& nums) {
-        int N = nums.size();
+        int n=nums.size();
 
-        vector<int> lisLength(N, 1);
-        vector<int> ldsLength(N, 1);
+        int minimumsteps=INT_MAX;
+         
+         lengthOfLIS(nums);
 
-        // Stores the length of longest increasing subsequence that ends at i.
-        for (int i = 0; i < N; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    lisLength[i] = max(lisLength[i], lisLength[j] + 1);
-                }
-            }
+         for(auto it:dp1)
+         cout<<it<<" ";
+
+         cout<<endl;
+         lengthOfLDS(nums);
+
+         for(auto it:dp2)
+         cout<<it<<" ";
+
+         cout<<endl;
+        for(int i=0;i<n;i++){
+            minimumsteps=min(minimumsteps,n+1-(dp1[i]+dp2[i]));
         }
-
-        // Stores the length of longest decreasing subsequence that starts at i.
-        for (int i = N - 1; i >= 0; i--) {
-            for (int j = i + 1; j < N; j++) {
-                if (nums[i] > nums[j]) {
-                    ldsLength[i] = max(ldsLength[i], ldsLength[j] + 1);
-                }
-            }
-        }
-
-        int minRemovals = INT_MAX;
-        for (int i = 0; i < N; i++) {
-            if (lisLength[i] > 1 && ldsLength[i] > 1) {
-                minRemovals =
-                    min(minRemovals, N - lisLength[i] - ldsLength[i] + 1);
-            }
-        }
-
-        return minRemovals;
+         
+         return minimumsteps;
     }
 };
