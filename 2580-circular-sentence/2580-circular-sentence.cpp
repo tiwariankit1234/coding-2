70932a574fb86b8cc6d56f305a7541674a32a989
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-11-02 11:57:09 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.91%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,36 +1,52 @@
-class Solution {
-public:
-    bool isCircularSentence(string sentence) {
-        stringstream ss(sentence);
-        string s;
-       vector<string>ans;
+// use of front and last character 
 
-        while(ss>>s){
-            ans.push_back(s);
-        }
+class Solution {
+// public:
+//     // bool isCircularSentence(string sentence) {
+//     // //     stringstream ss(sentence);
+//     // //     string s;
+//     // //    vector<string>ans;
+
+//     // //     while(ss>>s){
+//     // //         ans.push_back(s);
+//     // //     }
 
 
        
-       int n=ans.size();
+//     // //    int n=ans.size();
 
 
-         if(ans[0].front()!=ans[ans.size()-1].back())
-        return false;
+//     // //      if(ans[0].front()!=ans[ans.size()-1].back())
+//     // //     return false;
 
 
-       char last=ans[0].back();
-        for(int i=1;i<n;i++){
+//     // //    char last=ans[0].back();
+//     // //     for(int i=1;i<n;i++){
            
-          if(ans[i].front()!=last)return false;
+//     // //       if(ans[i].front()!=last)return false;
   
-         last=ans[i].back();
+//     // //      last=ans[i].back();
 
+//     // //     }
+
+//     // //     if(ans[0].front()!=ans[ans.size()-1].back())
+//     // //     return false;
+
+//     // //     return true;
+
+//     // }
+
+public:
+      bool isCircularSentence(string s){
+        int n=s.size();
+        
+        if(s[0]!=s[n-1])return false;
+
+        for(int i=0;i<n;i++){
+            if(s[i]==' ' and s[i-1]!=s[i+1])
+            return false;
         }
-
-        if(ans[0].front()!=ans[ans.size()-1].back())
-        return false;
-
-        return true;
-
-    }
+         
+         return true;
+      }
 };
