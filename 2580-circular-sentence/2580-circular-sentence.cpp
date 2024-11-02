724f87144bf59af06d35ed48c352a120478c8fd4
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-11-02 11:42:45 +0530

    Time: 2 ms (14.55%), Space: 9.2 MB (16.96%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -8,8 +8,15 @@ public:
         while(ss>>s){
             ans.push_back(s);
         }
+
+
        
        int n=ans.size();
+
+
+         if(ans[0].front()!=ans[ans.size()-1].back())
+        return false;
+
 
        char last=ans[0].back();
         for(int i=1;i<n;i++){
