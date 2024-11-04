c764da9613a77799a360c47d7d7e7522f0e59411
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-11-04 16:31:33 +0530

    Time: 0 ms (100%), Space: 33.5 MB (56.46%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -9,20 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     TreeNode* searchBST(TreeNode* root, int val) {
+//         if(root==NULL)return NULL;
+      
+       
+//      TreeNode* left=searchBST(root->left,val);
+//         if(root->val==val)return root;
+//         if(left)return left;
+       
+//       TreeNode* right=searchBST(root->right,val);
+//        if(right)
+//        return right;
+     
+//      return NULL;
+//     }
+// };
+
+
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(root==NULL)return NULL;
+        if(root==NULL || root->val==val)return root;
       
-       
-     TreeNode* left=searchBST(root->left,val);
-        if(root->val==val)return root;
-        if(left)return left;
-       
-      TreeNode* right=searchBST(root->right,val);
-       if(right)
-       return right;
-     
-     return NULL;
+       return root->val>val? searchBST(root->left,val):searchBST(root->right,val);
+
     }
+    
 };
+
