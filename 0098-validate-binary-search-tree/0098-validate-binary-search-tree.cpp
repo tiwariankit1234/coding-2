2755d293a057e3f3a9c47f6152e7997f0c362bf1
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-11-07 20:35:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,9 +6,18 @@ public:
     }
     bool validate(TreeNode* node, TreeNode* prev) {
         if (node == NULL) return true;
-        if (!validate(node->left, prev)) return false;
+
+
+        if (!validate(node->left,prev)) return false;
         if (prev != NULL && prev->val >= node->val) return false;
-        prev = node;
-        return validate(node->right, prev);
+    //    if(node)cout<<node->val<<" ";
+    //    if(prev)cout<<prev->val<<" ";
+    //    cout<<endl;
+        prev=node;
+        bool t=validate(node->right, prev);
+        // if(node->right)cout<<node->right->val<<" ";
+        // if(prev)cout<<prev->val<<" ";
+        // cout<<endl;
+        return t;
     }
 };
