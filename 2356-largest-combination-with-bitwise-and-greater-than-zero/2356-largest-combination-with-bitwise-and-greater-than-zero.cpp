b290fd0f6d27446970121ddde4f537b35ea3186c
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-11-07 22:14:59 +0530

    Time: 39 ms (27.61%), Space: 60.3 MB (14.16%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -3,26 +3,55 @@ public:
     int largestCombination(vector<int>& nums) {
         vector<int>ans(32,0);
         int count=0,value=0;
+        for(int i=0;i<24;i++){
         for(int num:nums){
-            count=0;
-            while(num>0){
-               
-                if(num%2==1){
-                    value=1;
-                }
-                else{
-                    value=0;
-                }
-                ans[count]+=value;
-                count++;
-                num/=2;
-            }
+            //check if the i-th bit is set
+            if((num&(1<<i))!=0) 
+            ans[i]++;
         }
-        int size=0;
-        for(auto it:ans){
-          size=max(size,it);
-        }
-        return size;
 
+        }
+       
+        return *max_element(ans.begin(),ans.end());
     }
 };
+
+
+
+
+
+// #include <vector>
+// #include <map>
+
+// class Solution {
+// public:
+//     std::map<std::pair<int, int>, int> memo;
+
+//     int f(const std::vector<int>& num, int idx, int previousand) {
+//         if (previousand == 0) return 0;  // If AND becomes zero, return 0
+//         if (idx == num.size()) return 0; // Reached end of array
+
+//         // Check if result is already computed
+//         if (memo.find({idx, previousand}) != memo.end()) {
+//             return memo[{idx, previousand}];
+//         }
+
+//         // Option to pick the current element
+//         int currentand = previousand & num[idx];
+//         int pick=0;
+//         if(currentand>0)
+//          pick = 1 + f(num, idx + 1, currentand);
+
+//         // Option to skip the current element
+//         int nonpick = f(num, idx + 1, previousand);
+
+//         // Store and return the maximum of the two options
+//         return memo[{idx, previousand}] = std::max(pick, nonpick);
+//     }
+
+//     int largestCombination(std::vector<int>& num) {
+//         // Set `previousand` to a high value (all bits set to 1)
+//         int all_ones = ~0;
+//         return f(num, 0, all_ones);
+//     }
+// };
