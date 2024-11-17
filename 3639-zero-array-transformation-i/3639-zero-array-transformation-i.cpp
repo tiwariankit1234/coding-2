b54a4917bca30545a194177b6885cfd3f7e8847b
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-11-17 21:48:19 +0530

    Time: 12 ms (100%), Space: 294.3 MB (63.64%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -10,7 +10,7 @@ public:
         }
         prefixarray[0]=linemapping[0];
 
-        for(auto it:linemapping)cout<<it<<" ";
+        // for(auto it:linemapping)cout<<it<<" ";
 
         cout<<endl;
 
@@ -24,7 +24,7 @@ public:
           prefixarray[i]=prefixarray[i-1]+linemapping[i];
         }
 
-         for(auto it:prefixarray)cout<<it<<" ";
+        //  for(auto it:prefixarray)cout<<it<<" ";
          
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
