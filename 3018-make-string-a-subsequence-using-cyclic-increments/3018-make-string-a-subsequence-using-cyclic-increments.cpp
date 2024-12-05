0589110f08870e462efde71fa3ae2e6e13cd96ce
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-05 22:47:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s1, string s2) {
-        int i=0,j=0;
-        int n1=s1.size(),n2=s2.size();
+        int n1 = s1.size(), n2 = s2.size();
+        
+        // DP table
+        vector<vector<bool>> dp(n2 + 1, vector<bool>(n1 + 1, false));
 
-        while(i<n1 and j<n2){
-            if(s1[i]==s2[j] || ((s1[i]-'a'+1)%26)==(s2[j]-'a')){
-                i++;
-                j++;
-            }
-            else{
-                i++;
+        // Base cases
+        for (int j = 0; j <= n1; j++) dp[0][j] = true; // Empty s2 is a subsequence of any prefix of s1
+
+        // Fill DP table
+        for (int i = 1; i <= n2; i++) {
+            for (int j = 1; j <= n1; j++) {
+                if (s1[j - 1] == s2[i - 1] || ((s1[j - 1] - 'a' + 1) % 26) == (s2[i - 1] - 'a')) {
+                    dp[i][j] = dp[i - 1][j - 1]; // Match and move both pointers
+                } else {
+                    dp[i][j] = dp[i][j - 1]; // Skip s1[j-1] and move in s1
+                }
             }
         }
-        return (j==n2);
+
+        // Return final result
+        return dp[n2][n1];
     }
 };
