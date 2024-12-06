e21d3c41d27113246b62be537027ece29225b9eb
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-06 11:22:02 +0530

    Time: 1724 ms (5.13%), Space: 37.6 MB (8.4%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -1,35 +1,75 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
-        int startLength = start.size();
-        // Pointer for start string and target string
-        int startIndex = 0, targetIndex = 0;
+       // intialise two queue
+       int n=start.size(),k=target.size();
+       if(n!=k)return false;
 
-        while (startIndex < startLength || targetIndex < startLength) {
-            // Skip underscores in start
-            while (startIndex < startLength && start[startIndex] == '_') {
-                startIndex++;
-            }
-            // Skip underscores in target
-            while (targetIndex < startLength && target[targetIndex] == '_') {
-                targetIndex++;
-            }
-            // If one string is exhausted, both should be exhausted
-            if (startIndex == startLength || targetIndex == startLength) {
-                return startIndex == startLength && targetIndex == startLength;
-            }
+       queue<pair<char,int>>q1,q2;
+       
+       for(int i=0;i<n;i++){
+        if(start[i]!='_'){
+        q1.push({start[i],i});
+        }
+       }
 
-            // Check if the pieces match and follow movement rules
-            if (start[startIndex] != target[targetIndex] ||
-                (start[startIndex] == 'L' && startIndex < targetIndex) ||
-                (start[startIndex] == 'R' && startIndex > targetIndex))
+        for(int i=0;i<k;i++){
+        if(target[i]!='_'){
+        q2.push({target[i],i});
+        }
+       }
+
+    //  std::queue<pair<char,int>> temp = q1; // Create a copy of the queue
+
+    // std::cout << "Queue contents: ";
+    // while (!temp.empty()) {
+    //     std::cout << temp.front().first << " "<<temp.front().second; // Print the front element
+    //     temp.pop();                       // Remove the front element
+    // }
+
+    //  std::queue<pair<char,int>>prev = q2; // Create a copy of the queue
+
+    // std::cout << "Queue contents: ";
+    // while (!prev.empty()) {
+    //     std::cout << prev.front().first<< " "<<prev.front().second; // Print the front element
+    //     prev.pop();                       // Remove the front element
+    // }
+
+       if(q1.size()!=q2.size())return false;
+       
+          while(q1.size()>0){
+            char ch1=q1.front().first;
+            int index1=q1.front().second;
+
+            char ch2=q2.front().first;
+            int index2=q2.front().second;
+
+            q1.pop();
+            q2.pop();
+
+            if(ch1!=ch2 ){
+                cout<<ch1<<endl;
+                cout<<index1<<endl;
+            return false;
+            }
+             
+             else if(ch1=='L'){
+
+                cout<<index1<<" "<<index2<<endl;
+                if(index1<index2)
                 return false;
 
-            startIndex++;
-            targetIndex++;
-        }
+             }
 
-        // If all conditions are satisfied, return true
-        return true;
+             else if(ch1=='R'){
+                cout<<index1<<" "<<index2<<endl;
+                if(index1>index2)
+                return false;
+             }
+ 
+          }
+
+          return true;
+        
     }
 };
