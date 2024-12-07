9f790a9755cacee7c9f3b6dca44a1e0b9ebb3c42
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-07 22:35:47 +0530

    Time: 35 ms (54.67%), Space: 59.9 MB (15.54%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,15 +1,12 @@
-//https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/
-
 class Solution {
 public:
-        // penalty lies from 1 to max try each penalty and calculate operations if operations jyada aa rahe kisi penalty par matlab toh right side jao//
-        // i want min(max)
+
       bool possible(int mid,vector<int>& nums,int maxoperations){
         int operations=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
             
-            operations+=((nums[i])/mid);
+            operations+=((nums[i]-1)/mid);
         
         }
         cout<<"operations"<<" "<<operations<<endl;
