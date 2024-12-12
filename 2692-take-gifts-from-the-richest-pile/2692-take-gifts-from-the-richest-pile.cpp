da294502c215e3c5eeb5ac9ccfe31eacc5e62c71
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-12 11:42:01 +0530

    Time: 4 ms (31.5%), Space: 13 MB (89.48%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -5,21 +5,22 @@ public:
     long long pickGifts(vector<int>& gifts, int k) {
         ll n=gifts.size();
         priority_queue<ll,vector<ll>,less<ll>>pq;
+
+        ll sum=0;
         for(auto it:gifts){
             pq.push(it);
+            sum+=it;
         }
 
         while( k){
            ll maxelement=pq.top();
             pq.pop();
+            sum-=maxelement;
             pq.push(sqrt(maxelement));
+            sum+=sqrt(maxelement);
             k--;
         }
-        ll sum=0;
-        while(pq.size()>0){
-           sum+=pq.top();
-           pq.pop();
-        }
+      
         return sum;
     }
 };
