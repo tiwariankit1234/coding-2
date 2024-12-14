1b84034e93e4942c6779509bd2d48ffb50ed0e09
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-14 11:44:35 +0530

    Time: 115 ms (20.21%), Space: 106.6 MB (18.62%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        // Sort based on the end points of the balloons
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
-
-        // Track the position of the previous arrow
-        int prev = points[0][1];
-        int count = 1; // At least one arrow is needed
-
-        // Iterate through the points
-        for (const auto& it : points) {
-            // If the start of the current balloon is beyond the previous arrow
-            if (it[0] > prev) {
+       
+        sort(points.begin(),points.end());
+        int minend=INT_MAX;
+        int count=1;
+        for(auto it:points){
+            if(it[0]>minend){  // overlapping condition update minend key parameter of the overlapping set 
                 count++;
-                prev = it[1]; // Update the position of the arrow
+                minend=it[1]; 
             }
+            else{
+                 minend=min(minend,it[1]);                          //agar nahi toh update the new state of overlapping set          
+            } 
         }
-
         return count;
     }
 };
