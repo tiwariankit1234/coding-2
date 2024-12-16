2a0cfe09d8920b3cce62cb370326a35f64092495
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-16 18:58:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.cpp
+++ 3523-find-the-power-of-k-size-subarrays-ii/3523-find-the-power-of-k-size-subarrays-ii.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>ans;
-        deque<int>dq;
-        int i=0,j=0;
-        while(j<n){
-            if(dq.empty() ||  dq.front()+1==nums[j]){
-                dq.push_front(nums[j]);
-            }
-            else{
-                dq.clear();
-                dq.push_front(nums[j]);
-            }
-            if(j-i+1==k){
-                if(dq.size()==k){
-                    ans.push_back(dq.front());
+        int n = nums.size();
+        vector<int> ans;
+        deque<int> dq; // to track the current sliding window
 
-                }
-                else{
-                   ans.push_back(-1); 
-                }
-                dq.pop_back();
+        for (int i = 0; i < n; ++i) {
+            // Add current number to deque
+            if (dq.empty() || nums[i] == dq.back() + 1) {
+                dq.push_back(nums[i]);
+            } else {
+                // Clear deque and start fresh if sequence breaks
+                dq.clear();
+                dq.push_back(nums[i]);
             }
-            i++;
-            j++;
+
+            // Start processing the window once size reaches k
+            if (i >= k - 1) {
+                // Check if deque contains k elements (indicating consecutive sequence)
+                if (dq.size() == k) {
+                    ans.push_back(dq.back()); // Add the maximum in the current window
+                } else {
+                    ans.push_back(-1); // Sequence is not valid
+                }
+
+                // Remove the oldest element in the sliding window
+                if (!dq.empty() && dq.front() == nums[i - k + 1]) {
+                    dq.pop_front();
+                }
+            }
         }
+
         return ans;
     }
 };
