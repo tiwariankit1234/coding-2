4790ff586a16b3453792acac2ed1e9c96decee30
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-22 22:53:30 +0530

    Time: 6 ms (12.93%), Space: 28.1 MB (20.2%) - LeetHub

--- 3611-construct-the-minimum-bitwise-array-ii/3611-construct-the-minimum-bitwise-array-ii.cpp
+++ 3611-construct-the-minimum-bitwise-array-ii/3611-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
+int f(int num){
+     int k=-1;
+        for(int i=0;i<31;i++){
+        
+            //check if the i-th bit is set
+            if((num&(1<<i))==0) {
+               k=i;
+               break;
+            }
+           
+        }
+        
+
+    if(k!=-1 and (k-1)<31){
+            num=num^(1<<(k-1));
+        }
+        return num;
+}
     vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> ans(nums.size());
-
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-
-            if (num == 2) {
-                ans[i] = -1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==2){
+                nums[i]=-1;
                 continue;
             }
-
-            int numCopy = num;
-            int count = 0;
-
-            // Count consecutive 1s from the right
-            while ((num & 1) == 1) {
-                count++;
-                num >>= 1;
-            }
-
-            // [count]th bit is the position of the last 1, like 100111 (count = 3)
-            // Subtract the bit                                     ^
-            ans[i] = numCopy - (1 << (count - 1));
+            nums[i]=f(nums[i]);
         }
-
-        return ans;
+        return nums;
     }
 };
