f688080e25de8f8ad1bfd61b29d453d87862149a
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-27 08:37:25 +0530

    Time: 4 ms (23.39%), Space: 49.1 MB (8.8%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -3,27 +3,25 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n=values.size();
         vector<int>copy=values;
-        for(int i=n-1;i>=0;i--){
-            values[i]=values[i]-i;
-        }
-        for(auto it:values)
-        cout<<it<<" ";
+       
+        // for(auto it:values)
+        // cout<<it<<" ";
 
         cout<<endl;
         vector<int>maxposition(n,0);
-        int maxpost=values[n-1];
+        int maxpost=values[n-1]-(n-1);
         for(int i=n-2;i>=0;i--){
            
             maxposition[i]=maxpost;
-             maxpost=max(values[i],maxpost);
+             maxpost=max(values[i]-i,maxpost);
         }
-        for(auto it:maxposition)
-        cout<<it<<" ";
+        // for(auto it:maxposition)
+        // cout<<it<<" ";
 
         cout<<endl;
         int ans=0;
         for(int i=0;i<(n-1);i++){
-            int currentvalue=(copy[i]+i+maxposition[i]);
+            int currentvalue=(values[i]+i+maxposition[i]);
             ans=max(ans,currentvalue);
         }
         return ans;
