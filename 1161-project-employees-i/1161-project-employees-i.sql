7e4a4e9af5061845f7ed5012bac45cb28cb55ab1
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-29 11:59:25 +0530

    Time: 1427 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT 
+p.project_id,ROUND(SUM(e.experience_years)/(COUNT(e.experience_years)),2) as average_years
+
+FROM
+project as p,employee as e
+WHERE
+p.employee_id=e.employee_id
+GROUP BY
+p.project_id;
