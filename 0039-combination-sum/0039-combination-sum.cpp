5719586cb9b729f8d3f0ad4c933c774581bc3c84
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-30 23:20:38 +0530

    Time: 0 ms (100%), Space: 14.4 MB (42.47%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
 int n;
-
+// Every time you have N choice (since you can use number repeatedly), and you can choose at most target times (actually it's smaller than target), so we can assume the worst-case time complexity is O(N^target).
 vector<vector<int>>ans;
 void f(vector<int>& candidates,int target,int idx,vector<int>&temp){
-    if(idx==n){
-        if(target<0)
-        return ;
+       
+       if(target<0)return ;
         if(target==0){
             ans.push_back(temp);
             return;
         }
-        return ;
-    }
+        
+    
     // cout<<target<<endl;
-    if(candidates[idx]<=target){
-      temp.push_back(candidates[idx]);
-    //  int t=target-candidates[idx];
-    f(candidates,target-candidates[idx],idx,temp);
-    temp.pop_back();
+    // if(candidates[idx]<=target){
+    //   temp.push_back(candidates[idx]);
+    // //  int t=target-candidates[idx];
+    // f(candidates,target-candidates[idx],idx,temp);
+    // temp.pop_back();
+    // }
+    // f(candidates,target,idx+1,temp);
+    for(int i=idx;i<n;i++){
+       temp.push_back(candidates[i]);
+        f(candidates,target-candidates[i],i,temp);
+        temp.pop_back();
     }
-    f(candidates,target,idx+1,temp);
     return ;
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
