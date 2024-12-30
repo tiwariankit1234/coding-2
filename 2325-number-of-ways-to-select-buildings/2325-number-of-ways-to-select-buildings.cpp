346455e4141056e68ab9b405260bdbc85cfdf397
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-31 00:33:03 +0530

    Time: 279 ms (15.58%), Space: 46.1 MB (43.72%) - LeetHub

--- 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.cpp
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
+    long long dp[100001][4][3]; // Memo table for DP
+
+    long long count(int index, string &s, int size, int last) {
+        if (index >= s.size()) {
+            return size == 3 ? 1 : 0;
+        }
+
+        if (dp[index][size][last] != -1) {
+            return dp[index][size][last];
+        }
+
+        long long ways = 0;
+
+        // Option 1: Include current character if valid
+        if (size < 3 && (size == 0 || s[index] - '0' != last)) {
+            ways += count(index + 1, s, size + 1, s[index] - '0');
+        }
+
+        // Option 2: Skip current character
+        ways += count(index + 1, s, size, last);
+
+        return dp[index][size][last] = ways;
+    }
+
     long long numberOfWays(string s) {
-        long long a=0,b=0,ans=0;        // a and b are the number of occurances of '1' and '0' after the current building respectively.
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1')
-                a++;
-            else
-                b++;
-        }
-        long long c=0,d=0;              // c and d are the number of occurances of '1' and '0' before the current building respectively.
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1'){               // Counting the sequences of "010"
-                ans+=(d*b);
-                a--;
-                c++;
-            }
-            else{                        // Counting the sequences of "101"
-                ans+=(a*c);
-                b--;
-                d++;
-            }
-        }
-        return ans;
+        memset(dp, -1, sizeof(dp)); // Initialize DP table with -1
+        return count(0, s, 0, 2);   // Start with index 0, size 0, and invalid last character
     }
 };
