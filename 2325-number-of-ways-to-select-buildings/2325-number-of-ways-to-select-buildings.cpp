e19fd0d0c87f3671bf756d58e9e117e8d89adccf
tiwariankit1234 <ankittiwari002003@gmail.com>
2024-12-31 10:51:08 +0530

    Time: 34 ms (60.55%), Space: 27.7 MB (68.34%) - LeetHub

--- 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.cpp
+++ 2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.cpp
@@ -1,31 +1,36 @@
+#define ll long long 
 class Solution {
 public:
-    long long dp[100001][4][3]; // Memo table for DP
-
-    long long count(int index, string &s, int size, int last) {
-        if (index >= s.size()) {
-            return size == 3 ? 1 : 0;
-        }
-
-        if (dp[index][size][last] != -1) {
-            return dp[index][size][last];
-        }
-
-        long long ways = 0;
-
-        // Option 1: Include current character if valid
-        if (size < 3 && (size == 0 || s[index] - '0' != last)) {
-            ways += count(index + 1, s, size + 1, s[index] - '0');
-        }
-
-        // Option 2: Skip current character
-        ways += count(index + 1, s, size, last);
-
-        return dp[index][size][last] = ways;
-    }
-
     long long numberOfWays(string s) {
-        memset(dp, -1, sizeof(dp)); // Initialize DP table with -1
-        return count(0, s, 0, 2);   // Start with index 0, size 0, and invalid last character
+     ll totalzero=0,totalone=0;
+     ll n=s.size();
+     // for 010 count zero
+     ll leftzero=0,leftone=0,count=0;
+     for(int i=0;i<n;i++){
+        if(s[i]=='0'){
+            totalzero++;
+        }
+        else{
+            totalone++;
+        }
+     }
+     for(int i=0;i<n;i++){
+       if(s[i]=='1'){
+         count+=leftzero*(totalzero-leftzero);
+       }
+      else{
+        leftzero++;
+      }
+     }
+ //101
+      for(int i=0;i<n;i++){
+       if(s[i]=='0'){
+         count+=leftone*(totalone-leftone);
+       }
+      else{
+        leftone++;
+      }
+     }
+     return count;
     }
 };
