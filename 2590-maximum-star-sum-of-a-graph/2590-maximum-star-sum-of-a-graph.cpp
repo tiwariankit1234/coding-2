fbca06c225ceb75bd4408507d4f7577696f233a9
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-01 23:21:48 +0530

    Time: 102 ms (43.79%), Space: 136 MB (59.25%) - LeetHub

--- 2590-maximum-star-sum-of-a-graph/2590-maximum-star-sum-of-a-graph.cpp
+++ 2590-maximum-star-sum-of-a-graph/2590-maximum-star-sum-of-a-graph.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     int maxStarSum(vector<int>& vals, vector<vector<int>>& edges, int k) {
-        int n = vals.size();
-        int maxsum = INT_MIN;
-        vector<vector<int>> adj(n);
-
-        // Build adjacency list
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        int n=vals.size(),sum=0,maxsum=INT_MIN;
+        vector<vector<int>>adj(n);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-
-        // Iterate through each node
-        for (int i = 0; i < n; i++) {
-            // Sort neighbors based on their values in descending order
-            sort(adj[i].begin(), adj[i].end(), [&](int a, int b) {
-                return vals[a] > vals[b];
-            });
-
-            // Compute star sum
-            int sum = vals[i]; // Start with the value of the current node
-            for (int j = 0; j < k && j < adj[i].size(); j++) {
-                if (vals[adj[i][j]] > 0) { // Only add positive contributions
-                    sum += vals[adj[i][j]];
-                }
+        // for(auto it:adj){
+        //     for(auto k:it){
+        //         cout<<k<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=0;i<n;i++){
+          if(adj[i].size()>0)  sort(adj[i].begin(),adj[i].end(),[&](int a,int b){
+            return vals[a]>vals[b];
+          });
+        //   for(auto it:adj[i]){
+        //     cout<<it<<" ";
+        //   }
+        //   cout<<endl;
+            int j=0;
+            int l=k;
+            int sum=vals[i];
+            // while(l-- and adj[i].size()==k){
+            //    int node=adj[i][j++];
+            //   if(i==3)cout<<node<<" ";
+            //    sum+=vals[node];
+            // }
+            for(int j=0;j<k and j<adj[i].size();j++){
+                if(vals[adj[i][j]]>0)
+                sum+=vals[adj[i][j]];
             }
-
-            // Update the maximum star sum
-            maxsum = max(maxsum, sum);
+           
+          
+            // cout<<sum<<endl;
+            maxsum=max(maxsum,sum);
         }
-
         return maxsum;
     }
 };
