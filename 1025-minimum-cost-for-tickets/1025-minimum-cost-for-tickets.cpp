0aaf58cc3febd0900e199d25c11f64ee09b36a45
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-01 09:34:41 +0530

    Time: 0 ms (100%), Space: 13.4 MB (24.16%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    unordered_set<int> isTravelNeeded;
-    
-    int solve(vector<int>& dp, vector<int>& days, vector<int>& costs, int currDay) {
-        // If we have iterated over travel days, return 0.
-        if (currDay > days[days.size() - 1]) {
-            return 0;
-        }
-        
-        // If we don't need to travel on this day, move on to next day.
-        if (isTravelNeeded.find(currDay) == isTravelNeeded.end()) {
-            return solve(dp, days, costs, currDay + 1);
-        }
-        
-        // If already calculated, return from here with the stored answer.
-        if (dp[currDay] != -1) {
-            return dp[currDay];
-        }
-        
-        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
-        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
-        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
-        
-        // Store the cost with the minimum of the three options.
-        return dp[currDay] = min(oneDay, min(sevenDay, thirtyDay));
+int dp[366];
+int f(int idx,vector<int>& cost,vector<int>& day){
+    int n=day.size();
+    if(idx==n){
+        return 0;
     }
+    if(dp[idx]!=-1)return dp[idx];
+    int cost1=cost[0]+(f(idx+1,cost,day));
+    int cost2;
+    cost2=cost[1];
+    int index=upper_bound(day.begin(),day.end(),day[idx]+6)-day.begin();
+  
     
+        cost2+=f(index,cost,day);
+    
+      index=upper_bound(day.begin(),day.end(),day[idx]+29)-day.begin();
+      int cost3=cost[2];
+      
+    
+        cost3+=f(index,cost,day);
+      
+           return dp[idx]=min({cost1,cost2,cost3});
+}
+
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        // The last day on which we need to travel.
-        int lastDay = days[days.size() - 1];
-        vector<int> dp(lastDay + 1, -1);
-        
-        // Mark the days on which we need to travel.
-        for (int day : days) {
-            isTravelNeeded.insert(day);
-        }
-        
-        return solve(dp, days, costs, 1);
+        memset(dp,-1,sizeof(dp));
+        return f(0,costs,days);
     }
 };
