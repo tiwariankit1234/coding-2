a757daa71dce936a95233ff912d8a21231bdfec8
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-03 19:11:43 +0530

    Time: 91 ms (5.49%), Space: 88.1 MB (51.85%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,34 @@
+#define ll long long
+
+
+class Solution {
+public:
+
+ bool possible(vector<int>& candies,ll k,ll mid){
+    ll operations=0;
+    ll n=candies.size();
+    for(ll i=0;i<n;i++){
+        operations+=((candies[i])/mid);
+    }
+    if(operations>=k)return true;
+    return false;
+ }
+
+    ll maximumCandies(vector<int>& candies, long long k) {
+        sort(candies.begin(),candies.end());
+        ll low=1,high=1e9;
+        ll ans=0;
+        while(low<=high){
+            ll mid=(low+high)/2;
+            if(possible(candies,k,mid)){
+                 ans=mid;
+                low=mid+1;
+              
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
