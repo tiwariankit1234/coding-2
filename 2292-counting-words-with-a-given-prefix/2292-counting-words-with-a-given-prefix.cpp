4b6883bf7315839064a13ada943f91b2d63090a3
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-09 08:06:58 +0530

    Time: 0 ms (100%), Space: 13.6 MB (34.5%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -3,8 +3,8 @@ public:
     int prefixCount(vector<string>& words, string pref) {
         int count=0;
         for(auto it:words){
-            if(pref.size()>it.size())
-            continue;
+            // if(pref.size()>it.size())
+            // continue;
 
             if(it.find(pref)==0)
             count++;
