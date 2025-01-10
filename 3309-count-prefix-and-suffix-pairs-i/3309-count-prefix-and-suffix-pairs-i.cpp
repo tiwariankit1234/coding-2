9ffe4c7a7228585de54e739305d747fab8cd06a5
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-10 08:09:03 +0530

    Time: 14 ms (7.63%), Space: 24.8 MB (6.74%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-bool check(string s,string t){
-    if(t.size()<s.size())return false;
-    int l1=0,r1=0,l2=s.size()-1,r2=t.size()-1;
-    bool flag=true;
-    for( l1,r1,l2,r2;(l1<(s.size()) and r1<(t.size()) and flag and (l2>=(0)) and (r2>=0) );l1++,r1++,l2--,r2--){
-        if(s[l1]!=t[r1]){
-            flag=false;
-        }
-        if(s[l2]!=t[r2])
-        flag=false;
-    }
-    return flag;
-   
-
-}
     int countPrefixSuffixPairs(vector<string>& words) {
-        int n=words.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(check(words[i],words[j])){
-                    count++;
-                }
+        ios::sync_with_stdio(false);
+        int ans = 0;
+        unordered_map<string, int> mp;
+
+        for (int x = words.size() - 1; x >= 0; x--) {
+            string word = words[x];
+            int wordLength = word.size();
+            
+            string prefix = "", suffix = "";
+            
+            if (mp.find(word) != mp.end()) {
+                ans += mp[word];
             }
+
+            for (int i = 0; i < wordLength ; i++) {
+                prefix += word[i];            
+                suffix = word[wordLength - 1 - i] + suffix;  
+                if (prefix == suffix) {
+                    mp[prefix]++;
+                } 
+                cout<<prefix<<" "<<suffix;
+            }
+  
+         cout<<endl;
         }
-        return count;
+
+        return ans;
     }
 };
