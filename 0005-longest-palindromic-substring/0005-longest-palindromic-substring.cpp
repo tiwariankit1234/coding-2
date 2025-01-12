16839b04d26af38df019ae36f9391d29b18e7fd6
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-12 20:09:04 +0530

    Time: 182 ms (28.53%), Space: 13.7 MB (60.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n=s.size();
-        bool dp[1002][1002]={false};
-     
-       pair<int,int>ans;
-        for(int i=0;i<n;i++){
-            
-                dp[i][i]=true;
-                if(i<n-1){
-                if(s[i]==s[i+1]){
-                    dp[i][i+1]=true;
-                    ans={i,i+1};
-                  
-                }
-                else{
-                    dp[i][i+1]=false;
-                }
-            }
-            
-        } 
-
-      
-
-            // now we fill the dp with 1 and 2 length 
-            for(int diff=2;diff<n;diff++){
-                for(int i=0;i<n-diff;i++){
-                    int j=i+diff;
-                    if(s[i]==s[j] and dp[i+1][j-1]){
-                        dp[i][j]=true;
-                        ans={i,j};
-                    }
-                }
-            }
+    int dp[1002][1002];
+    
+    bool f(int i, int j, string &s) {
+        if (i >= j) {
+            return dp[i][j] = 1;
+        }
         
-        int i=ans.first;
-        int j=ans.second;
-        return s.substr(i,j-i+1);
-
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
+        // If characters match, check inner substring
+        if (s[i] == s[j]) {
+            return dp[i][j] = f(i + 1, j - 1, s);
+        }
+        
+        // If characters don't match, it's not a palindrome
+        return dp[i][j] = 0;
+    }
+    
+    string longestPalindrome(string s) {
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
+        
+        int maxlength = 1;  // Initialize with 1 since single character is palindrome
+        int startidx = 0;
+        
+        // Check all possible substrings
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (f(i, j, s) == 1 && (j - i + 1) > maxlength) {
+                    maxlength = j - i + 1;
+                    startidx = i;
+                }
+            }
+        }
+        
+        return s.substr(startidx, maxlength);
     }
 };
