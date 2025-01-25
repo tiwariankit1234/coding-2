6ac0a0b6bde2d8d28a049166eb6c6ca47085bdfc
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-25 19:38:14 +0530

    Time: 223 ms (6.05%), Space: 88.1 MB (26.33%) - LeetHub

--- 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2351-number-of-ways-to-buy-pens-and-pencils/2351-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -1,19 +1,45 @@
 #define ll long long 
 class Solution {
 public:
-    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        ll count=0;
-        count+=total/cost2+1;
-        // cout<<count<<endl;
-        while(total>0){
-           total-=cost1;
-           if(total==0)
-           count+=1;
-           else if(total>0){
-            count+=total/cost2+1;
-           }
-        }
+ll dp[1000005][2];
+ll f(int total,vector<int>&cost,int idx){
+    // cout<<total<<" "<<cost[idx]<<" "<<idx<<endl;
+    if(total<0)return 0;
+      if(total==0)return 0;
+    if(idx==1){
+        return (total)/cost[idx];
+    }
 
-        return count;
+        if(dp[total][idx]!=-1)return dp[total][idx];
+
+  
+
+      ll take=0;
+    if(total-cost[idx]>=0){
+     take=1+f(total-cost[idx],cost,idx);
+    }
+   ll notake=f(total,cost,idx+1);
+    return dp[total][idx]=take+notake;
+}
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        // ll count=0;
+        // count+=total/cost2+1;
+        // // cout<<count<<endl;
+        // while(total>0){
+        //    total-=cost1;
+        //    if(total==0)
+        //    count+=1;
+        //    else if(total>0){
+        //     count+=total/cost2+1;
+        //    }
+        // }
+
+        // return count;
+        // vector<vector<int>>dp(total+1,vector<int>(2,-1));
+        vector<int>cost(2,0);
+        memset(dp,-1,sizeof(dp));
+        cost[0]=cost1;
+        cost[1]=cost2;
+        return 1+f(total,cost,0);
     }
 };
