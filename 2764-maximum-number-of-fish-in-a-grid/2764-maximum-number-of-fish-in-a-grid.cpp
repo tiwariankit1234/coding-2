b07e4a7f075de3c96bb1528babec33701d94e4c8
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-01-28 21:34:00 +0530

    Time: 222 ms (5.08%), Space: 187.6 MB (5.08%) - LeetHub

---
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+int dir[4][2]={{-1,0},{0,-1},{1,0},{0,1}};
+bool isvalid(int row,int col,vector<vector<int>>& grid,vector<vector<int>>&visited){
+    int m=grid.size();
+    int n=grid[0].size();
+    if((row>=m || col>=n|| row<0||col<0) || grid[row][col]==0 || visited[row][col]==1)return false;
+    else
+    return true;
+}
+int dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&visited){
+   
+    int m=grid.size();
+    int n=grid[0].size();
+    int count=grid[i][j];
+    visited[i][j]=1;
+
+    //   cout<<i<<" "<<j<<endl;
+    for(int k=0;k<4;k++){
+        int nrow=i+dir[k][0];
+        int ncol=j+dir[k][1];
+        // cout<<"nrow"<<nrow<<" "<<"ncol"<<ncol<<endl;
+        if(isvalid(nrow,ncol,grid,visited)){
+            // cout<<"nrow"<<nrow<<" "<<"ncol"<<ncol<<endl;
+            count+=dfs(nrow,ncol,grid,visited);
+        }
+    }
+  
+    return count;
+
+}
+
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        int count=0;
+        int m=grid.size(),n=grid[0].size();
+       
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                vector<vector<int>>visited(m,vector<int>(n,0));
+                if(grid[i][j]){
+                    // cout<<"startpos"<<i<<" "<<"startpos"<<j<<endl;
+                    count=max(count,dfs(i,j,grid,visited));
+                }
+            }
+        }
+        return count;
+    }
+};
