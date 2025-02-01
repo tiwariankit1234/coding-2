6457547997ab591997411f5171db5b2c838fb4f0
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-02-01 18:13:25 +0530

    Time: 5 ms (27.2%), Space: 14.2 MB (29.7%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+int repeat=0;
+bool  dfs(int node, vector<vector<int>>&adj,vector<int>&visited,vector<int>&cycle,int parent){
+    visited[node]=1;
+    // cout<<node<<endl;
+    for(auto child:adj[node]){
+        if(visited[child]==0){
+           bool t=dfs(child,adj,visited,cycle,node);
+         if(t==true){
+            if(repeat==node){
+                cycle[node]=true;
+                return false;
+            }
+            cycle[node]=true;
+            return true;
+         }
+        }
+        else if(visited[child]==1 and child!=parent){
+            repeat=child;
+            cycle[node]=true;
+            return true;
+        }
+    }
+    return false;
+}
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+         int n = edges.size();
+           vector<vector<int>>adj(n+1);
+           vector<int>visited(n+1),cycle(n+1);
+           for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+           }
+         
+        dfs(1,adj,visited,cycle,-1);
+        for(int i=n-1;i>=0;i--){
+            if(cycle[edges[i][0]]==1 and cycle[edges[i][1]]==1)
+            return edges[i];
+        }
+        return {};
+    }
+};
