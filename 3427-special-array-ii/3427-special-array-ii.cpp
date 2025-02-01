cc65198462ece440f1a2cb5e02c04b2065c6fd1e
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-02-01 16:16:20 +0530

    Time: 45 ms (19.03%), Space: 135.8 MB (27.72%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> maxReach(n); // Initialize with size n
-        int end=0;
-        // Precompute the maxReach for each starting index
-        for (int start = 0; start < n; ++start) {
-             end = max(start,end);
-            while (end < n - 1 && (nums[end] % 2) != (nums[end + 1] % 2)) {
-                ++end;
+bool binary_search(int start,int end,vector<int>&indices){
+    int l=0,h=indices.size()-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(start<=indices[mid] and indices[mid]<=end)
+            return true;
+            else if(indices[mid]>end){
+                h=mid-1;
             }
-            maxReach[start] = end;
+            else{
+                l=mid+1;
+            }
         }
-
-        // Process each query
-        vector<bool> ans(queries.size());
-        for (int i = 0; i < queries.size(); ++i) {
-            int start = queries[i][0];
-            int end = queries[i][1];
-            ans[i] = (end <= maxReach[start]);
+     return false;
+}
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+         vector<int>indices;
+        for(int i=1;i<n;i++){
+            if(nums[i]%2==nums[i-1]%2){
+                indices.push_back(i);
+            }
         }
-
-        return ans;
+        vector<bool>ans(queries.size());
+        int j=0;
+       for(auto it:queries){
+          ans[j++]=!binary_search(it[0]+1,it[1],indices);
+       }
+       return ans;
     }
 };
