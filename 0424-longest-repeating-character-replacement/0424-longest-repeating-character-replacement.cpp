be8a659ad1026ccf9fe5350454c5c58718d17f1b
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-02-11 23:59:27 +0530

    Time: 7 ms (52.6%), Space: 10.9 MB (72.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
+// int f(string &s,int l,int r){
+//    int hash[26]={0};
+//     int mx=1;
+//     for(int i=l;i<=r;i++){
+//        hash[s[i]-'A']++;
+//         mx=max(mx,hash[s[i]-'A']);
+//     }
+//     return mx;
+// }
     int characterReplacement(string s, int k) {
-        unordered_map<char, int> mp;
-        int n = s.size();
-        int l = 0, r = 0, maxfrequency = 0, len = 0;
-        
-        while (r < n) {
-            mp[s[r]]++;
-            maxfrequency = max(maxfrequency, mp[s[r]]);
-            
-            // If window size - maxfrequency > k, shrink the window
-            while ((r - l + 1) - maxfrequency > k) {
-                mp[s[l]]--;
+        int n=s.size();
+        int l=0,r=0,maxfrequency=0,len=INT_MIN;
+          int hash[26]={0};
+        while(r<n){
+              hash[s[r]-'A']++;
+            maxfrequency=max(maxfrequency,hash[s[r]-'A']);
+            // cout<<maxfrequency<<" ";
+            while(((r-l+1)-maxfrequency)>k){
+                hash[s[l]-'A']--;
                 l++;
+                maxfrequency=0;
+                   
+   
+    for(int i=0;i<26;i++){
+       
+        maxfrequency=max(maxfrequency,hash[i]);
+    }
+   
             }
-            
-            // Update the maximum length found
-            len = max(len, r - l + 1);
+            // cout<<l<<" "<<r<<endl;
+            len=max(len,(r-l+1));
             r++;
         }
         
