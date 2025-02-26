2aea83ecc4d32d74a42e0830b0e408f2b2e6f27b
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-02-26 12:34:40 +0530

    Time: 8 ms (35.57%), Space: 111.9 MB (79.87%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,50 +1,20 @@
+int  mod= 1e9+7;
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        const int MOD = 1e9 + 7;
-        int n = arr.size();
-
-        // Convert elements to 0 (even) or 1 (odd)
-        for (auto& num : arr) {
-            num %= 2;
-        }
-
-        // dpEven[i] represents the number of subarrays with an even sum ending
-        // at index i dpOdd[i] represents the number of subarrays with an odd
-        // sum ending at index i
-        vector<int> dpEven(n), dpOdd(n);
-
-        // Initialization based on the last element
-        // The last element is even
-        if (arr[n - 1] == 0) {
-            dpEven[n - 1] = 1;
-        } else {
-            // The last element is odd
-            dpOdd[n - 1] = 1;
-        }
-
-        // Traverse the array in reverse
-        for (int num = n - 2; num >= 0; num--) {
-            if (arr[num] == 1) {
-                // Odd element contributes to odd subarrays
-                dpOdd[num] = (1 + dpEven[num + 1]) % MOD;
-                // Even element continues the pattern
-                dpEven[num] = dpOdd[num + 1];
-            } else {
-                // Even element contributes to even subarrays
-                dpEven[num] = (1 + dpEven[num + 1]) % MOD;
-                // Odd element continues the pattern
-                dpOdd[num] = dpOdd[num + 1];
+        int n=arr.size();
+        int oddcount=0,evencount=1,prefixsum=0,cnt=0;
+        for(int i=0;i<n;i++){
+            prefixsum+=arr[i];
+            if(prefixsum&1){    // sum is odd see in the remaining array      how many evencount is there     
+            oddcount++;
+            cnt=(((cnt%mod)+(evencount)%mod)%mod);
+            }
+            else{
+                evencount++;
+                cnt=((cnt%mod)+(oddcount%mod)%mod);
             }
         }
-
-        // Sum all the odd subarrays
-        int count = 0;
-        for (auto oddCount : dpOdd) {
-            count += oddCount;
-            count %= MOD;
-        }
-
-        return count;
+        return cnt;
     }
 };
