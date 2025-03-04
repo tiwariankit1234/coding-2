0f7a671d9406661c75dc3d9e8a60ca2ceea8aa7d
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-04 10:25:31 +0530

    Time: 11 ms (38.19%), Space: 32.7 MB (28.3%) - LeetHub

---
+++ 3705-find-the-largest-almost-missing-integer/3705-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/find-the-largest-almost-missing-integer/
+
+// look for edge case k=1 k=n and k variable case
+//  always think of different edge cases 
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> subarray_count;
+        
+        // Process all subarrays of size k
+        for (int i = 0; i <= n - k; ++i) {
+            unordered_set<int> unique_nums(nums.begin() + i, nums.begin() + i + k);
+            for (int num : unique_nums) {
+                subarray_count[num]++;
+            }
+        }
+        
+        // Find the largest integer that appears in exactly one subarray
+        int ans = -1;
+        for (const auto& [num, count] : subarray_count) {
+            if (count == 1 && num > ans) {
+                ans = num;
+            }
+        }
+        
+        return ans;
+    }
+};
