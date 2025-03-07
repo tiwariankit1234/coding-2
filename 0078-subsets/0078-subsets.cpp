55803ff50257036fef2cde239b5075040cec0f6f
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-07 19:30:44 +0530

    Time: 0 ms (100%), Space: 10 MB (46.09%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> output;
-    int n, k;
+    int n;
     void backtrack(int first, vector<int> &curr, vector<int>& nums) {
         // if the combination is done
-        if (curr.size() == k) output.push_back(curr);
+           output.push_back(curr);
+        
         for (int i = first; i < n; ++i) {
             // add i into the current combination
             curr.push_back(nums[i]);
+            
             // use next integers to complete the combination
             backtrack(i + 1, curr, nums);
             // backtrack
             curr.pop_back();
         }
+        return ;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         n = nums.size();
-        for (k = 0; k < n + 1; ++k) {
-            vector<int> curr;
+        vector<int> curr;
             backtrack(0, curr, nums);
-        }
+        
         return output;
     }
 };
