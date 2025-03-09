3b2d68944f0633a21112cb8ea71dff2e71ae7970
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-09 19:30:00 +0530

    Time: 87 ms (55.02%), Space: 63 MB (7.05%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // at max O(N2) calls to the functions
+    bool isPalindrome(string& s, int l, int r, vector<vector<int>>& dp){
+        if(l >= r) return true;
+        if(dp[l][r] != -1) return dp[l][r];
+        if(s[l] == s[r]) return dp[l][r] = isPalindrome(s, l+1, r-1, dp);
+        return dp[l][r] = false;
+    }
+    int n;
+    int performMinCuts(string& s, int index, vector<int>& dp, vector<vector<int>>& pDp){
+        if(index == n){
+            return 0;
+        }
+        int minCuts = 100000;
+        if(dp[index] != -1) return dp[index];
+        // no need to partition, if substr is already palindrome
+        if(isPalindrome(s, index, n-1, pDp)) return dp[index] = 0; 
+        for(int i = index; i < n; i++){
+            if(isPalindrome(s, index, i, pDp)){
+                // try to cut from here
+                minCuts = min(minCuts, 1 + performMinCuts(s, i+1, dp, pDp));
+            }
+        }
+        return dp[index] = minCuts;
+    }
+
+    int minCut(string s) {
+        n = s.size();
+        vector<int> dp(n, -1); // dp for minCut problem
+        vector<vector<int>> pDp(n, vector<int>(n, -1)); // dp for palindrome
+        return performMinCuts(s, 0, dp, pDp);
+    }
+};
