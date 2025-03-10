30a77567ad777af20eef5cf02e7e3d3d56185e69
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-10 10:20:35 +0530

    Time: 17 ms (24.12%), Space: 17.1 MB (30.15%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>words(wordDict.begin(),wordDict.end());
-        queue<int>q;
-        vector<bool>seen(s.length(),false);
-        int start=0;
-        q.push(start);
-        int n=s.length();
-        while(q.size()>0){
-            int start=q.front();
-            q.pop();
-            
-            if(start==s.length())return true;
-            for(int end=start+1;end<=n;end++){
-
-                 if (seen[end]) {
-                    continue;
-                }
-                if(words.find(s.substr(start,end-start))!=words.end()){
-                    q.push(end);
-                    seen[end]=true;
-                }
-            }
-       
+unordered_set<string>st;
+vector<int>dp;
+bool f(int idx,string &s){
+   int n=s.size();
+   if(idx==n)return true;
+   if(dp[idx]!=-1)return dp[idx];
+   
+    for(int j=idx;j<n;j++){
+        string t=s.substr(idx,j-idx+1);
+        if(st.find(t)!=st.end() and f(j+1,s)){
+            dp[idx]=1;
+            return true;
         }
+        else{
+          continue;
+        }   
+    }    
+        dp[idx]=0;
         return false;
+}
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.size();
+          for(auto it:wordDict)
+          st.insert(it);
+
+          
+        dp.clear();
+        dp.resize(n+1,-1);
+        return f(0,s);
     }
 };
