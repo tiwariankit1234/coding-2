6eb6bd11f6ca417aaf00c6abb1efeb60c11ccb5e
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-10 17:15:08 +0530

    Time: 1122 ms (10.39%), Space: 10.7 MB (68.85%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
-    bool f(int i, int j, int index, string &word, vector<vector<bool>> &visited, vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        
-        // If we've matched all characters in the word
-        if (index == word.size()) {
-            return true;
-        }
-        
-        // Out of bounds or already visited or character doesn't match
-        if (i < 0 || i >= m || j < 0 || j >= n || visited[i][j] || grid[i][j] != word[index]) {
-            return false;
-        }
-        
-        // Mark as visited
-        visited[i][j] = true;
-        
-        // Try all four directions
-        bool found = f(i+1, j, index+1, word, visited, grid) || 
-                     f(i, j+1, index+1, word, visited, grid) ||
-                     f(i-1, j, index+1, word, visited, grid) ||
-                     f(i, j-1, index+1, word, visited, grid);
-        
-        // Backtrack
-        visited[i][j] = false;
-        
-        return found;
-    }
-    
+bool f(int i,int j,string &s,string& word,vector<vector<bool>>&visited,vector<vector<char>>& grid){
+      int m=grid.size(),n=grid[0].size();
+     if(s.size()==word.size()){
+        return s==word;
+     }
+   
+     if(i>=m ||i<0||j<0||j>=n||visited[i][j]==true) return  false;
+       bool ans=false;
+       visited[i][j]=true;
+      s+=grid[i][j];
+      ans=ans||f(i+1,j,s,word,visited,grid)||f(i,j+1,s,word,visited,grid)
+    ||f(i-1,j,s,word,visited,grid)||f(i,j-1,s,word,visited,grid);
+       visited[i][j]=false;
+       s.pop_back();
+     return ans;
+
+}
     bool exist(vector<vector<char>>& grid, string word) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+       
+        int m=grid.size(),n=grid[0].size();
+         vector<vector<bool>>visited(m,vector<bool>(n,false));
+         string s="";
         
-        // Try starting from each cell
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == word[0] && f(i, j, 0, word, visited, grid)) {
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==word[0] and f(i,j,s,word,visited,grid)){
                     return true;
                 }
             }
         }
-        
         return false;
     }
 };
