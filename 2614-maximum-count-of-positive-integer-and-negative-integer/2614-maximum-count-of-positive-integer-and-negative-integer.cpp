bef41e3d8f75f485643fce40b2123d0d65cf4eb0
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-12 10:20:08 +0530

    Time: 0 ms (100%), Space: 21.3 MB (75.85%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -5,9 +5,9 @@ public:
         int posidx=upper_bound(nums.begin(),nums.end(),0)-nums.begin();
          pos=n-posidx;
          int negidx=upper_bound(nums.begin(),nums.end(),-1)-1-nums.begin();
-         if(negidx==-1)
-         neg=0;
-         else
+        //  if(negidx==-1)
+        //  neg=0;
+        //  else
          neg=negidx+1;
 
          return max(pos,neg);
