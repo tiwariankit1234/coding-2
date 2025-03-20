c33bb318dc9a50f7b132fa9c887ab5e5ff726165
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-20 16:12:04 +0530

    Time: 0 ms (100%), Space: 30.5 MB (79.82%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int findUnsortedSubarray(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return 0;
-        vector<int>arr=nums;
-        sort(arr.begin(),arr.end());
-        int l=0,r=n-1;
-        while(l<n){
-            if(nums[l]==arr[l])
-            l++;
-            else
-            break;
+    int findUnsortedSubarray(vector<int>& arr) {
+        int n = arr.size();
+        int l = -1, r = -1;
+        for(int i = 0; i < n - 1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                l = i;
+                break;
+            }    
         }
-           cout<<l<<endl;
-        while(r>=0){
-            if(nums[r]==arr[r])
-          r--;
-            else
-            break;
+        for(int i = n - 1; i >= 1; i--) {
+            if(arr[i] < arr[i - 1]) {
+                r = i;
+                break;
+            }
         }
-        if(l>=r)return 0;
-        return r-l+1;
+        if(l == -1) return 0; // this means array is already sorted.
+        int mini = arr[l], maxi = arr[l];
+        for(int i = l; i <=r; i++) {
+            mini = min(mini, arr[i]);
+            maxi = max(maxi, arr[i]);
+        }
+        l = upper_bound(arr.begin(), arr.begin() + l, mini) - arr.begin();
+        r = lower_bound(arr.begin() + r + 1, arr.end(), maxi) - arr.begin();
+        return r - l;
     }
 };
