77bb55b429bd6794bde1fe6a5af306dbb1e1e9dc
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-21 22:06:01 +0530

    Time: 245 ms (53.52%), Space: 76.2 MB (63.45%) - LeetHub

---
+++ 1830-count-good-meals/1830-count-good-meals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mod = (7+1e9);
+    int countPairs(vector<int>& array) {
+        int n=array.size();
+        unordered_map<int, int> mpp;
+        long int count=0;
+        
+        for(int i=0; i<n; i++){
+          
+            
+            //For every element try & find the element if present such that the sum will be power of two.
+            //Constraints: max value = 2^20 & taking sum of two elements of max value: 2*2^20
+            //Check if there's present (2^0 - array[i]) or (2^1 - array[i]) or ... (2^21 - array[i])
+            for(int x=0; x<=21; x++){
+                  //Do not count the current element again. Example: 2+2
+                if(mpp.find((1<<x) - array[i])!=mpp.end()) count += mpp[((1<<x) - array[i])];
+               //Reset.
+            }
+                mpp[array[i]]++; 
+        }
+        
+        return (count%mod);
+    }
+};
