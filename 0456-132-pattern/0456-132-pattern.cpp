c1222711e014c715bb2b0ad0db568382a2564f68
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-22 08:52:57 +0530

    Time: 15 ms (31.85%), Space: 72.7 MB (14.8%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         int n=nums.size();
-        int l=nums[0];
-        map<int,int>r;
-        if(n<3) return false;
-        
-        for(int i=n-1;i>=2;i--)
-        {
-            r[nums[i]]++;
+        vector<int>small(n,0);
+        small[0]=nums[0];
+        for(int i=1;i<n;i++)
+        small[i]=min(nums[i],small[i-1]);
+
+        stack<int>st;
+        int index=-1;
+        for(int i=n-1;i>=0;i--){
+            if(small[i]!=nums[i]){
+                index=i;
+              st.push(nums[i]);
+              break;
+            }
         }
-        for(int i=1;i<n-1;i++)
-        {
-          int f=l;
-          
-          auto it2=r.upper_bound(f);
-          int f2=it2->first;
-          if( it2!=r.end() && f<f2 && f2<nums[i])
-          {
-            return true;
-          }
-          
-          else
-          {
-            l=min(l,nums[i]);
-            r[nums[i+1]]--;
-            if(r[nums[i+1]]==0) r.erase(nums[i+1]);
-          }
-        }
-        return false;
+       for(int i=index-1;i>=0;i--){
+         if(nums[i]==small[i])continue;
+         while(!st.empty() and st.top()<=small[i]){
+            st.pop();
+         }
+         if(!st.empty() and nums[i]>st.top()and nums[i]!=small[i])
+        return true;
+        else
+        st.push(nums[i]);
+       }
+            return false;
     }
 };
