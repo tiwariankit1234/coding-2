49e542548dcd734eb17e22dae24d39b9409455d6
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-25 16:25:59 +0530

    Time: 313 ms (6.92%), Space: 176.4 MB (5.71%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int countDays(int days, vector<vector<int>>& meetings) {
-        int freeDays = 0, latestEnd = 0;
-
-        // Sort meetings based on starting times
-        sort(meetings.begin(), meetings.end());
-
-        for (auto& meeting : meetings) {
-            int start = meeting[0], end = meeting[1];
-
-            // Add current range of days without a meeting
-            if (start > latestEnd + 1) {
-                freeDays += start - latestEnd - 1;
-            }
-
-            // Update latest meeting end
-            latestEnd = max(latestEnd, end);
+    int countDays(int days, vector<vector<int>>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        map<int,int>mp;
+        for(auto it:arr){
+        mp[it[0]]++;
+        mp[it[1]+1]--;
         }
-
-        // Add all days after the last day of meetings
-        freeDays += days - latestEnd;
-
-        return freeDays;
+        int count=0,sum=0;
+        vector<int>ans;
+        int previousday=-1,start=0;
+        for(auto it:mp){
+           if(previousday<=days and sum==0 and start>=1){
+              count+=it.first-previousday;
+           }
+             sum+=it.second;
+            if(sum==0){
+             start++;
+            previousday=it.first;
+            }
+          
+        }
+           previousday=previousday-1;
+        if(previousday<=days){
+            count+=days-previousday;
+        }
+        count+=arr[0][0]-1;
+        return count;
+        
     }
 };
