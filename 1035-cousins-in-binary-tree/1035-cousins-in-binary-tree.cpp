2a59e24edb8f97eaf5de7664fe84cf9d527aac3a
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-26 22:21:16 +0530

    Time: 3 ms (5.59%), Space: 14.4 MB (11.9%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.cpp
@@ -1,25 +1,22 @@
 
 class Solution {
 public:
-pair<int,int >p,q;
+// pair<int,int >p,q;
+unordered_map<int,pair<int,int>>mp;
 void f(TreeNode* root,int x,int y,int depth,int parent){
     if(!root)return ;
-    if(root->val==x){
-        p.first=depth;
-        p.second=parent;
-    }
-    else if(root->val==y){
-       q.first=depth;
-       q.second=parent;
-    }
+     mp[root->val]={depth,parent};
     f(root->left,x,y,depth+1,root->val);
     f(root->right,x,y,depth+1,root->val);
     return;
 }
     bool isCousins(TreeNode* root, int x, int y) {
         f(root,x,y,0,-1);
-        if(p.first==q.first and p.second!=q.second)return true;
-        else
-        return false;
+        // if(p.first==q.first and p.second!=q.second)return true;
+        // else
+        // return false;
+         if(mp[x].first==mp[y].first and mp[x].second!=mp[y].second)return true;
+         else
+         return false;
     }
 };
