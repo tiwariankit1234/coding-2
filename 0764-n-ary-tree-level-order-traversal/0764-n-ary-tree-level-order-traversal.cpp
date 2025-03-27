fb3d3c372c383e80295cc97bb965779dc32ffbf8
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-27 19:34:41 +0530

    Time: 3 ms (100%), Space: 16.3 MB (18.21%) - LeetHub

---
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,68 @@
+//https://leetcode.com/problems/n-ary-tree-level-order-traversal/?envType=problem-list-v2&envId=2tb72ufm
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+// emplace back ka use
+// ans vector me new level insert karne ke liye emplace_back() ka use karte hain
+// ans[level].push_back insert karne ke liye
+class Solution { // 16 ms, faster than 98.53%
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> levelOrder(Node* root) {
+        dfs(root, 0);
+        return ans;
+    }
+    void dfs(Node* root, int level) {
+        if (root == nullptr) return;
+        if (level == ans.size()){ ans.emplace_back();
+        cout<<level<<" "<<ans.size()<<endl;
+        } // Need more level -> Add new level
+        ans[level].push_back(root->val);
+        for (Node* child : root->children)
+            dfs(child, level + 1);
+    }
+};
+
+
+
+//bfs solution
+
+
+// //class Solution { 
+// public:
+//     vector<vector<int>> levelOrder(Node* root) {
+//         if (root == nullptr) return {};
+//         queue<Node*> q;
+//         q.push(root);
+//         vector<vector<int>> ans;
+//         while (!q.empty()) {
+//             ans.emplace_back();
+//             for (int i = q.size(); i >= 1; i--) {
+//                 Node* curr = q.front(); q.pop();
+//                 ans.back().push_back(curr->val);
+//                 for (Node* child : curr->children) {
+//                     q.push(child);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+//
