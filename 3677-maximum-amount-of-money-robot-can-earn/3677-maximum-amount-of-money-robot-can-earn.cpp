1e5807fdc5c9988c99112f4ee7c68f71c5825bc1
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-29 12:05:03 +0530

    Time: 683 ms (28.52%), Space: 275.9 MB (53.76%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int solve(int i, int j, int canNeutralize, vector<vector<int>>& coins, vector<vector<vector<int>>>& dp) {
+    int solve(int i, int j, int canNeutralize, vector<vector<int>>& coins,
+              vector<vector<vector<int>>>& dp) {
         int m = coins.size();
         int n = coins[0].size();
 
-        if (i >= m || j >= n) return -1000000;
+        if (i >= m || j >= n)
+            return -1e9;
         if (i == m - 1 && j == n - 1) {
             return (coins[i][j] < 0 && canNeutralize > 0) ? 0 : coins[i][j];
         }
-
-        if (dp[i][j][canNeutralize] != -1) {
+        if (dp[i][j][canNeutralize] != INT_MIN) {
             return dp[i][j][canNeutralize];
         }
 
-        int right = -1000000, down = -1000000;
+        int ans = -1e9;
 
         if (coins[i][j] < 0) {
             if (canNeutralize > 0) {
-                right = solve(i, j + 1, canNeutralize - 1, coins, dp);
-                down = solve(i + 1, j, canNeutralize - 1, coins, dp);
+                ans = max(ans,
+                          max(solve(i + 1, j, canNeutralize - 1, coins, dp),
+                              solve(i, j + 1, canNeutralize - 1, coins, dp)));
             }
-
-            right = max(right, solve(i, j + 1, canNeutralize, coins, dp) + coins[i][j]);
-            down = max(down, solve(i + 1, j, canNeutralize, coins, dp) + coins[i][j]);
-        } else {
-            right = solve(i, j + 1, canNeutralize, coins, dp) + coins[i][j];
-            down = solve(i + 1, j, canNeutralize, coins, dp) + coins[i][j];
         }
 
-        dp[i][j][canNeutralize] = max(right, down);
+                    ans=max(ans,max(coins[i][j]+solve(i+1,j,canNeutralize,coins,dp),coins[i][j]+solve(i,j+1,canNeutralize,coins,dp)));
+
+        dp[i][j][canNeutralize] = ans;
+
         return dp[i][j][canNeutralize];
     }
 
@@ -36,7 +35,8 @@ public:
         int m = coins.size();
         int n = coins[0].size();
 
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(
+            m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
         return solve(0, 0, 2, coins, dp);
     }
 };
