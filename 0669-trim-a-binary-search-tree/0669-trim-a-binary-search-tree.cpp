f9228366fe12e5fc5447def07e8464b83bbb8873
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-30 20:28:46 +0530

    Time: 0 ms (100%), Space: 24.9 MB (57.69%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -3,22 +3,21 @@ class Solution {
 public:
 // function return the trim binary search tree
  TreeNode* f(TreeNode* root,int low,int high){
+    if(!root)return NULL;
       if(root->val<low){
-        if(root->right)return  f(root->right,low,high);
-        else{
-            return NULL;
-        }
+       return  f(root->right,low,high);
+        
       }
         else if(root->val>high){
-            if(root->left)return f(root->left,low,high);
-            return NULL;
+            return f(root->left,low,high);
+               
         }
        else{
          if(root->left)root->left=f(root->left,low,high);
          if(root->right)root->right=f(root->right,low,high);
          return root;
        }
-       return root;
+     return root;
       }
  
     TreeNode* trimBST(TreeNode* root, int low, int high) {
