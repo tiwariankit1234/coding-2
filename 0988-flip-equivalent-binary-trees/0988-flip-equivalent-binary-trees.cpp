7749a06f35b3546e7c43d44704ee52f4abf78f3f
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-03-31 09:15:13 +0530

    Time: 0 ms (100%), Space: 15.3 MB (75.7%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.cpp
@@ -1,26 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+bool f(TreeNode* root1,TreeNode* root2){
+  if(!root1 and !root2)return true;
+  if(root1 and !root2 ||root2 and !root1)return false;
+
+
+  if(root1->val!=root2->val)return false;
+
+  bool t=f(root1->left,root2->left) and f(root1->right,root2->right);
+  bool k=f(root1->left,root2->right) and f(root1->right,root2->left);
+  return t||k;
+}
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        // Both trees are empty
-        if (root1 == nullptr && root2 == nullptr) {
-            return true;
-        }
-        // Just one of the trees is empty
-        if (root1 == nullptr || root2 == nullptr) {
-            return false;
-        }
-        // Corresponding values differ
-        if (root1->val != root2->val) {
-            return false;
-        }
-
-        // Check if corresponding subtrees are flip equivalent
-        bool noSwap = flipEquiv(root1->left, root2->left) &&
-                      flipEquiv(root1->right, root2->right);
-        // Check if opposite subtrees are flip equivalent
-        bool swap = flipEquiv(root1->left, root2->right) &&
-                    flipEquiv(root1->right, root2->left);
-
-        return swap || noSwap;
+        return f(root1,root2);
     }
 };
