eec68fced35f478f3b4bf010ee5b3c8efbbcb2a4
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-04-03 08:12:00 +0530

    Time: 11 ms (24.23%), Space: 98.9 MB (25.35%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -1,33 +1,16 @@
-#define ll long long 
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        ll n=nums.size();
-        vector<ll>prefixmax(n),suffixmax(n);
-        prefixmax[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixmax[i]=max((ll)nums[i],prefixmax[i-1]);
+        int n = nums.size();
+        vector<int> leftMax(n), rightMax(n);
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - i]);
         }
-        // for(auto it:prefixmax)
-        // cout<<it<<" ";
-
-        // cout<<endl;
-          suffixmax[n-1]=nums[n-1];
-          for(int i=n-2;i>=0;i--){
-            suffixmax[i]=max((ll)nums[i],suffixmax[i+1]);
-          }
-
-        //    for(auto it:suffixmax)
-        // cout<<it<<" ";
-
-        // cout<<endl;
-
-        ll ans=0;
-          for(int i=1;i<n-1;i++){
-           
-         
-            ans=max((prefixmax[i-1]-nums[i])*suffixmax[i+1],ans);
-          }
-          return ans;
+        long long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = max(res, (long long)(leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
     }
 };
