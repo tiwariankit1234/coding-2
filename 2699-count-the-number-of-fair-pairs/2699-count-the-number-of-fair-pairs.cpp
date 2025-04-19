a6fb08252023cd3d13e30481b616ee07c6a791fe
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-04-19 23:16:13 +0530

    Time: 58 ms (49.83%), Space: 60.5 MB (38.74%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,25 +1,33 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
+    long long lower_bound(vector<int>& nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return low;
+    }
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
         long long ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            // Assume we have picked nums[i] as the first pair element.
 
-        for (int i = 0; i < n - 1; i++) {
-            // Search only in the subarray starting from i+1
-            auto low = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
-            auto high = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]);
+            // `low` indicates the number of possible pairs with sum < lower.
+            int low =
+                lower_bound(nums, i + 1, nums.size() - 1, lower - nums[i]);
 
-          
-            
-            // Calculate the number of valid pairs with nums[i]
-            ans += ((high-1) - (low-1));
+            // `high` indicates the number of possible pairs with sum <= upper.
+            int high =
+                lower_bound(nums, i + 1, nums.size() - 1, upper - nums[i] + 1);
+
+            // Their difference gives the number of elements with sum in the
+            // given range.
+            ans += 1LL * (high - low);
         }
-        
         return ans;
     }
 };
