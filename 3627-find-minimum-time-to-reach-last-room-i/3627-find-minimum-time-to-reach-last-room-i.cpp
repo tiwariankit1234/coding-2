c1ba4e803a3e0738a31a7d0467bd563fee280c7a
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-05-10 23:52:31 +0530

    Time: 19 ms (76.07%), Space: 28.8 MB (85.34%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -1,46 +1,47 @@
-class State {
-public:
-    int x;
-    int y;
-    int dis;
-    State(int x, int y, int dis) : x(x), y(y), dis(dis) {}
-};
-
 class Solution {
 public:
-    int minTimeToReach(vector<vector<int>>& moveTime) {
-        int inf = 0x3f3f3f3f;
+    int dx[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    
+    int dijkstra(int x, int y, vector<vector<int>>& moveTime) {
         int n = moveTime.size(), m = moveTime[0].size();
-        vector<vector<int>> d(n, vector<int>(m, inf));
-        vector<vector<int>> v(n, vector<int>(m, 0));
-
-        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        d[0][0] = 0;
-
-        auto cmp = [](const State& a, const State& b) { return a.dis > b.dis; };
-
-        priority_queue<State, vector<State>, decltype(cmp)> q(cmp);
-        q.push(State(0, 0, 0));
-
-        while (!q.empty()) {
-            State s = q.top();
-            q.pop();
-            if (v[s.x][s.y]) continue;
-            v[s.x][s.y] = 1;
-
-            for (int i = 0; i < 4; i++) {
-                int nx = s.x + dirs[i][0];
-                int ny = s.y + dirs[i][1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-
-                int dist = max(d[s.x][s.y], moveTime[nx][ny]) + 1;
-                if (d[nx][ny] > dist) {
-                    d[nx][ny] = dist;
-                    q.push(State(nx, ny, dist));
+        
+        priority_queue< pair<int, pair<int, int>>, vector< pair<int, pair<int, int>> >, greater<> > pq;
+        
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[x][y] = 0;
+        pq.push({0, {x, y}});
+        
+        while (!pq.empty()) {
+            auto [dis, pos] = pq.top();
+            pq.pop();
+            
+            int cx = pos.first, cy = pos.second;
+            
+          
+            
+            for (int k = 0; k < 4; k++) {
+                int nx = cx + dx[k][0];
+                int ny = cy + dx[k][1];
+                
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    int extraCost = 1; // Move right/left cost 1, up/down cost 2
+                    int arriveTime =max( dis,moveTime[nx][ny]);
+                    arriveTime+=extraCost;
+               
+                    
+                    if (arriveTime < dist[nx][ny]) {
+                        dist[nx][ny] = arriveTime;
+                        // cout<<nx<<" "<<ny<<" "<<arriveTime<<" "<<moveTime[nx][ny]<<endl;
+                        pq.push({arriveTime, {nx, ny}});
+                    }
                 }
             }
         }
-
-        return d[n - 1][m - 1];
+        
+        return dist[n-1][m-1];
+    }
+    
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        return dijkstra(0, 0, moveTime);
     }
 };
