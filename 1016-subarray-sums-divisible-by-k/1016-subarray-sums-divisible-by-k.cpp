f17872f2f9eef77990e330fc26b864dfd32b2af7
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-05-31 12:53:26 +0530

    Time: 232 ms (5.29%), Space: 35.5 MB (73.09%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n=nums.size();
-
-        int prefixmod=0,result=0;
-
-        vector<int>modgroups(k);
-        modgroups[0]=1;
-
-        for(int num:nums){
-            prefixmod=(prefixmod+num%k+k)%k;
-
-            result+=modgroups[prefixmod];
-            modgroups[prefixmod]++;
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int currentsum=0,count=0;
+        for(int i=0;i<n;i++){
+            currentsum+=nums[i];
+            
+            if(mp.find(((currentsum-k)%k+k)%k)!=mp.end()){
+                count+=mp[((currentsum-k)%k+k)%k];
+            }
+            mp[((currentsum-k)%k+k)%k]+=1;
+          
+            cout<<endl;
         }
-        return result;
+        return count;
     }
 };
