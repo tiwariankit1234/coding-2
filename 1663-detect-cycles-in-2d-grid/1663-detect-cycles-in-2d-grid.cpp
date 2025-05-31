e3e08a668bc8f08d155e73275151eed5815d6479
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-05-31 17:48:19 +0530

    Time: 1956 ms (5.02%), Space: 208.7 MB (14.12%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-    int dir[4][2] = {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+int dir[4][2]={{-1,0},{0,1},{0,-1},{1,0}};
 
-    bool bfs(int i, int j, char ch, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-        int m = grid.size(), n = grid[0].size();
-        visited[i][j] = true;
-
-        queue<pair<pair<int, int>, pair<int, int>>> q;
-        q.push({{i, j}, {-1, -1}});  // root has no parent
-
-        while (!q.empty()) {
-            auto [curr, parent] = q.front(); q.pop();
-            int x = curr.first, y = curr.second;
-            int px = parent.first, py = parent.second;
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k][0], ny = y + dir[k][1];
-
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == ch) {
-                    if (!visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        q.push({{nx, ny}, {x, y}});
-                    } else if (!(nx == px && ny == py)) {
-                        return true;
-                    }
-                }
-            }
+bool bfs(int i,int j,char ch,vector<vector<char>>&grid,vector<vector<bool>>&visited){
+    int m=grid.size(),n=grid[0].size();
+   
+    visited[i][j]=true;
+   queue<pair<pair<int,int>,pair<int,int>>>q;
+   q.push({{i,j},{-1,-1}});
+   while(q.size()){
+     auto [x,y]=q.front().first;
+     auto [p,r]=q.front().second;
+     q.pop();
+   
+     
+    //  cout<<x<<" "<<y<<" "<<p<<" "<<r<<" "<<ch<<endl;
+     for(int k=0;k<4;k++){
+        int newx=x+dir[k][0],newy=y+dir[k][1];
+        if(newx>=0 and newx<m and newy>=0 and newy<n and grid[newx][newy]==ch and  visited[newx][newy]==false){
+               q.push({{newx,newy},{x,y}});
+                visited[newx][newy]=true;
         }
+        else if(newx>=0 and newx<m and newy>=0 and newy<n and grid[newx][newy]==ch and visited[newx][newy]==true and (!(newx==p and newy==r))){
+            return true;
+        }
+     }
+   }
 
-        return false;
-    }
-
+    // cout<<i<<" "<<j<<" "<<i<<" "<<j<<" "<<ch<<endl;
+       return false;
+} 
     bool containsCycle(vector<vector<char>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
+        int m=grid.size(),n=grid[0].size();
+             vector<vector<bool>>visited(m,vector<bool>(n,false));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                
+                if(visited[i][j]==false and bfs(i,j,grid[i][j],grid,visited) ){
+                
+                return true;
+                }
+                else{
+                    cout<<i<<" "<<j<<endl;
+                }
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (!visited[i][j]) {
-                    if (bfs(i, j, grid[i][j], grid, visited)) {
-                        return true;
-                    }
                 }
             }
-        }
-
+        
         return false;
     }
 };
