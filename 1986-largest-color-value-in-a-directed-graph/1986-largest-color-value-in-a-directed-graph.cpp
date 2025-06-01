4f101ba77fc35f18f3c14eeeab6c1695a7ae369d
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-06-01 20:11:05 +0530

    Time: 430 ms (16.12%), Space: 186 MB (50.68%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.cpp
@@ -1,41 +1,59 @@
-// OJ: https://leetcode.com/contest/weekly-contest-240/problems/largest-color-value-in-a-directed-graph/
-// Author: github.com/lzl124631x
-// Time: O(V + E)
-// Space: O(V + E)
 class Solution {
-    typedef array<int, 26> T;
 public:
-    int largestPathValue(string C, vector<vector<int>>& E) {
-        unordered_map<int, vector<int>> G;
-        vector<int> indegree(C.size());
-        for (auto &e : E) {
-            G[e[0]].push_back(e[1]); // build graph
-            indegree[e[1]]++; // count indegrees
-        }
-        vector<T> cnt(C.size(), T{}); // cnt[i][j] is the maximum count of j-th color from the ancester nodes to node i.
-        queue<int> q;
-        for (int i = 0; i < C.size(); ++i) {
-            if (indegree[i] == 0) { // if this node has 0 indegree, we can use it as a source node
-                q.push(i);
-                cnt[i][C[i] - 'a'] = 1; // the count of the current color should be 1
-            }
-        }
-        int ans = 0, seen = 0;
-        while (q.size()) {
-            auto u = q.front();
-            q.pop();
-            int val = *max_element(begin(cnt[u]), end(cnt[u])); // we use the maximum of all the maximum color counts as the color value.
-            ans = max(ans, val);
-            ++seen;
-            for (int v : G[u]) {
-                for (int i = 0; i < 26; ++i) {
-                    cnt[v][i] = max(cnt[v][i], cnt[u][i] + (i == C[v] - 'a')); // try to use node `u` to update all the color counts of node `v`.
+
+ int topoSort(int V, vector<int> adj[],string &color)
+	{
+        int maxi=INT_MIN;
+        vector<int>indegree(V,0);
+        vector<vector<int>>cnt(V,vector<int>(26,0));
+		for (int i = 0; i < V; i++) {
+			for (auto it : adj[i]) {
+				indegree[it]++;
+			}
+		}
+          
+		queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+                cnt[i][color[i]-'a']=1;
+			}
+		}
+
+		vector<int> topo;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			topo.push_back(node);
+            int temp=*max_element(cnt[node].begin(),cnt[node].end());
+            maxi=max(maxi,temp);
+			// node is in your topo sort
+			// so please remove it from the indegree
+
+			for (auto it : adj[node]) {
+				indegree[it]--;
+                for(int j=0;j<26;j++){
+                    cnt[it][j]=max(cnt[it][j],cnt[node][j]);
                 }
-                if (--indegree[v] == 0) {
-                    q.push(v);
-                }
-            }
-        }
-        return seen < C.size() ? -1 : ans;
+                 cnt[it][color[it] - 'a'] = max(cnt[it][color[it] - 'a'], cnt[node][color[it] - 'a'] + 1);
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+
+		if(topo.size()<V)return -1;
+        return maxi;
+	}
+
+
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        int n=colors.size();
+        vector<int>adj[n];
+        for(auto it:edges)
+        adj[it[0]].push_back(it[1]);
+        
+       return topoSort(n,adj,colors);
+       // This correctly checks for a cycle
+         
+       
     }
 };
