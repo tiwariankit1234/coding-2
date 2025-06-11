809ba92fd20d5cf9c37b3067c796eb2de03a7cd0
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-06-11 16:27:53 +0530

    Time: 53 ms (50.74%), Space: 34.7 MB (42.23%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     string robotWithString(string s) {
-        unordered_map<char, int> cnt;
-        for (char c : s) {
-            cnt[c]++;
+        string t = "";
+        int n = s.size();
+        stack<char> st;
+
+        // Precompute min char from i to end
+        vector<char> minchar(n);
+        minchar[n - 1] = s[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minchar[i] = min(s[i], minchar[i + 1]);
         }
 
-        stack<char> stk;
-        string res;
-        char minCharacter = 'a';
-        for (char c : s) {
-            stk.emplace(c);
-            cnt[c]--;
-            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
-                minCharacter++;
-            }
-            while (!stk.empty() && stk.top() <= minCharacter) {
-                res.push_back(stk.top());
-                stk.pop();
+        for (int i = 0; i < n; i++) {
+            st.push(s[i]);
+
+            // Pop from stack if top <= min remaining char
+            while (!st.empty() && (i == n - 1 || st.top() <= minchar[i + 1])) {
+                t.push_back(st.top());
+                st.pop();
             }
         }
 
-        return res;
+        return t;
     }
 };
