4514328325c2a1e41319df34dc2584370ec1c54f
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-06-19 00:41:33 +0530

    Time: 508 ms (7.6%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,9 @@
-# Write your MySQL query statement be
-
-SELECT 
- e2.name
+# Write your MySQL query statement below
+SELECT e2.name 
 FROM 
-Employee as e1,
-Employee as e2
-WHERE
-e1.managerId=e2.id
+Employee as e1 INNER JOIN 
+Employee as e2 on e1.managerID =e2.id
 GROUP BY
-e2.id
-HAVING
-count(e2.id)>=5;
-
--- This is more optimized
+e1.managerID 
+HAVING 
+COUNT(e1.managerID)>=5;
