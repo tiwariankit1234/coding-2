3bece8b6600abb815acd04eb10a969e644f26a3f
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-04 18:04:19 +0530

    Time: 756 ms (38.75%), Space: 0B (100%) - LeetHub

---
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+WITH CTE as(
+    SELECT
+    product_id,order_date,unit
+    FROM
+    Orders
+    WHERE
+    order_date>='2020-02-01' and order_date<='2020-02-29'
+),
+CTE2 as(
+    SELECT 
+    product_id,SUM(unit) as total_units
+    FROM
+    CTE
+    GROUP BY
+    product_id
+    HAVING
+    total_units>=100 
+)
+SELECT 
+p.product_name as product_name,CTE2.total_units as unit
+FROM
+Products as p
+INNER JOIN
+CTE2
+ON
+p.product_id=CTE2.product_id;
