674e3bf2c38e5d338c5b680b7b72debf1e3e2a22
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-06 11:06:27 +0530

    Time: 314 ms (97.24%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -7,3 +7,9 @@ distinct visited_on,SUM(amount)OVER(ORDER BY visited_on RANGE BETWEEN INTERVAL 6
 FROM
 Customer)as radhe
 where visited_on>=first_date+6;
+
+-- over ()means it will work for all the rows
+-- another sql query
+-- select *, round(amount/7,2) as average_amount from(
+-- select distinct visited_on, sum(amount) over (ORDER BY visited_on RANGE -- BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) as amount
+-- from customer LIMIT 99999 OFFSET 6) t;
