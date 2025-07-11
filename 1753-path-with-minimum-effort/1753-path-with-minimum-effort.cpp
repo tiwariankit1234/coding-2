6d18e69f64d280cef4314900f0ff4f9a021853d9
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-11 18:13:34 +0530

    Time: 185 ms (12.96%), Space: 55.1 MB (11.22%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,45 +1,48 @@
-#define pii pair<int,int> 
+#define pii pair<int, int>
+
 class Solution {
 public:
     int dir[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    bool isValid(int x,int y,vector<vector<int>>& grid){
-        int m=grid.size(),n=grid[0].size();
-        return x>=0 and x<m and y>=0 and y<n;
+
+    bool isValid(int x, int y, int m, int n) {
+        return x >= 0 && x < m && y >= 0 && y < n;
     }
+
     bool possible(int threshold, vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        priority_queue<pii, vector<pii>, greater<pii>> pq;
-        vector<int> visited(m * n+n,false);
-         pq.push({0, 0});
-        int mini = INT_MAX;
-          visited[0]=true;
-        while (pq.size()) {
-            auto [cost, node] = pq.top();
-           
-            pq.pop();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pii> q;
+        q.push({0, 0});
+        visited[0][0] = true;
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            if (x == m - 1 && y == n - 1)
+                return true;
+
             for (int k = 0; k < 4; k++) {
-                int x = node / n, y = node % n;
-                if (x == m - 1 and y == n - 1)
-                    return true;
-                int newx = x + dir[k][0], newy = y + dir[k][1];
-                // cout<<cost<<" "<<node<<" "<<x<<" "<<y<<" "<<newx<<" "<<newy<<endl;
-                if (isValid(newx, newy, grid) and
-                    abs(grid[newx][newy] - grid[x][y]) <= threshold and
-                    visited[newx * n + newy] == false) {
-                    visited[newx * n + newy] = true;
-                    pq.push({threshold, newx * n + newy});
+                int newx = x + dir[k][0];
+                int newy = y + dir[k][1];
+                if (isValid(newx, newy, m, n) && !visited[newx][newy]) {
+                    int diff = abs(grid[newx][newy] - grid[x][y]);
+                    if (diff <= threshold) {
+                        visited[newx][newy] = true;
+                        q.push({newx, newy});
+                    }
                 }
             }
         }
+
         return false;
     }
+
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n = heights.size();
-        int low = 0, high = 1e6, ans = INT_MAX;
+        int low = 0, high = 1e6, ans = 0;
+
         while (low <= high) {
-            
-            int mid = (low + high)/2;
-            // cout<<low<<" "<<mid<<" "<<high<<endl;
+            int mid = (low + high) / 2;
             if (possible(mid, heights)) {
                 ans = mid;
                 high = mid - 1;
@@ -47,6 +50,7 @@ public:
                 low = mid + 1;
             }
         }
+
         return ans;
     }
 };
