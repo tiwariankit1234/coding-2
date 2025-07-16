8f731dc2ddc9821c30b255f90d3cab03904aa313
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-16 17:31:24 +0530

    Time: 272 ms (5.24%), Space: 42 MB (7.13%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,18 +1,33 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ unordered_map<int,int>mp;
 class Solution {
 public:
-  unordered_set<int>s;
+
+bool f(TreeNode* root,int k,unordered_map<int,int>&mp){
+     if(!root)return false;
+         cout<<root->val<<endl;
+        bool left=f(root->left,k,mp);
+        if(mp.find(k-root->val)!=mp.end()){
+            return true;
+        }
+       else mp[root->val]++;
+        bool right=f(root->right,k,mp);
+        cout<<left<<" "<<right<<endl;
+        return left||right;
+}
     bool findTarget(TreeNode* root, int k) {
-       if(!root)return false;
-       bool left= findTarget(root->left,k);
-       if(left)return true;
-       if(!s.count(k-root->val)) s.insert(root->val);
-       else
-       return true;
-       bool right=findTarget(root->right,k);
-       if(right)return right;
-       return false;
        
-        
+       unordered_map<int,int>mp;
+       return f(root,k,mp);
     }
 };
