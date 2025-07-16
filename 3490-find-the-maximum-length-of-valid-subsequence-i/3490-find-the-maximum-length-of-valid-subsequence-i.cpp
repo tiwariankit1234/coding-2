b15132d3a4e3da84eae193ac33c05a7e7d039ac1
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-16 23:04:29 +0530

    Time: 1315 ms (5%), Space: 99.9 MB (22.35%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]%2;
+        }
+        if(n==2)return 2;
+        int maxsubs=1;
+        vector<int>dp(n,1);
+        for(auto it:nums)
+        cout<<it<<" ";
+        cout<<endl;
+        int previdxzero=-1,previdxone=-1;
+        if(nums[0]==0)
+         previdxzero=0;
+        else
+        previdxone=0;
+        for(int i=1;i<=n-1;i++){
+            if(nums[i]==1 and previdxzero!=-1){
+                dp[i]=dp[previdxzero]+1;
+                maxsubs=max(dp[i],maxsubs);
+                previdxone=i;
+            }
+            else if(nums[i]==0 and previdxone!=-1){
+              dp[i]=dp[previdxone]+1;
+                maxsubs=max(dp[i],maxsubs);
+                previdxzero=i;
+            }
+            
+     
+        }
+        dp.clear();
+       dp.resize(n,1);
+
+    int zerosequence=0;
+    int onesequence=0;
+    for(auto i:nums){
+        if(i==0)
+        zerosequence++;
+        else
+        onesequence++;
+    }
+    maxsubs=max(max(zerosequence,onesequence),maxsubs);
+
+ return maxsubs;
+        
+    }
+};
