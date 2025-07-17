346122ce2288247c9b6c7ecbda9e535ea5e1a78a
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-17 23:36:54 +0530

    Time: 783 ms (12.39%), Space: 388.1 MB (5.01%) - LeetHub

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/3491-find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
 
-      int dp[1005][1004];
+     
     int maximumLength(vector<int>& nums, int k) {
         int n=nums.size();
         for(auto &x:nums)
@@ -11,20 +11,21 @@ public:
         // for(auto it:nums)
         // cout<<it<<" ";
 
-        cout<<endl;
+        // cout<<endl;
         int ans=INT_MIN;
-        memset(dp,-1,sizeof(dp));
+       vector<vector<int>>dp(1005,vector<int>(1005,1));
 
         for(int i=0;i<nums.size();i++){
           for(int j=0;j<=i-1;j++){
               int difference=(nums[i]+nums[j])%k;
                if((dp[j][difference]+1)>dp[i][difference]){
                 dp[i][difference]=1+dp[j][difference];
+                // cout<<dp[j][difference]<<" "<<dp[i][difference]<<endl;
                 ans=max(ans,dp[i][difference]);
               }
           }
         }   
-          return ans+2;
+          return ans;
 
     }
 };
