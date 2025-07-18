b073eb3aaedfa60757dca8d4ddf0a88c128d2e9a
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-18 22:00:43 +0530

    Time: 11 ms (64.23%), Space: 8.8 MB (88.02%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+int dp[502][502];
+int mod=1e9+7;
+int f(int idx,int steps,int n){
+    
+    // cout<<idx<<" "<<steps<<endl;
+    if(steps==0){
+     if(idx==0)return 1;
+     else
+     return 0;
+    }
+    if(dp[idx][steps]!=-1)return dp[idx][steps];
+    int left=0;
+    if(idx>=1){
+        left=f(idx-1,steps-1,n)%mod;
+    }
+    int right=0;
+    if(idx<(n-1)){
+        right=f(idx+1,steps-1,n)%mod;
+    }
+    int stay=f(idx,steps-1,n)%mod;
+    return dp[idx][steps]=(((stay+right)%mod+left)%mod)%mod;
+}
+    int numWays(int steps, int arrLen) {
+    
+        memset(dp,-1,sizeof(dp));
+        // cout<<"5"<<endl;
+        
+        return f(0,steps,arrLen);
+    }
+};
