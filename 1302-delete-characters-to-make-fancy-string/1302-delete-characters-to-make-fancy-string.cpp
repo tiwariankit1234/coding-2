2abcbf902ecf28827f60304ad052fe61bd6b9acc
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-22 00:23:00 +0530

    Time: 24 ms (71.86%), Space: 44.5 MB (67.47%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -2,35 +2,17 @@ class Solution {
 public:
     string makeFancyString(string s) {
         int n=s.size();
-        if(n<=2)return s;
-
-        int count=0;
+        int i=0;
         string newstring="";
-
-        for(int i=0;i<n;i++){
-            count=1;
-            for(int j=i+1;j<n;j++){
-                if(s[j]==s[i]){
-                  count++;
-                }
-                else{
-                    break;
-                }
-            }
-            if(count>=3){
-                 newstring+=s[i];
-                newstring+=s[i];
-                // cout<<i<<" ";
-                i=i+count-1;
-                // cout<<count<<" ";
-                // cout<<i<<" ";
-                 
-                //  cout<<endl;
-
-            }
-            else{
-                newstring+=s[i];
-            }
+        while(i<n){
+         if(i>=2 and s[i]==s[i-1]and s[i-1]==s[i-2]){
+            i++;
+         }
+         else{
+            newstring+=s[i];
+            i++;
+         }
+     
         }
         return newstring;
     }
