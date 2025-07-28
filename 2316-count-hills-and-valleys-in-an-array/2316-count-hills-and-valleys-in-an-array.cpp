fa8fdb819937fc0edddf54baa8fd13aff3fc108e
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-29 02:09:53 +0530

    Time: 0 ms (100%), Space: 12.8 MB (16.05%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -5,7 +5,8 @@ public:
 
         int n = nums.size();
         int counthills = 0, countvalleys = 0,left=nums[0];
-
+        // track karo ki continuous window me kabhi answer nahi ayyega
+        // toh hame unique element kab pakad sakte hain jab starting window ke left me unequal element mile 
         for (int i = 1; i < (n - 1); i++) {
             if (nums[i] != nums[i + 1]) {
                 if (left < nums[i] and nums[i] > nums[i + 1]) {
