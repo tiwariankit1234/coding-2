322c9fedbcdfedb85c48b9cc05c9435bda71c82f
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-07-30 16:51:02 +0530

    Time: 0 ms (100%), Space: 85.9 MB (90.15%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,61 +1,32 @@
 class Solution {
- public:
-//     int longestSubarray(vector<int>& nums) {
-//         // here we are going to see the maximum largest element subarray
-//         // find maximum element and its index
+public:
+    int longestSubarray(vector<int>& nums) {
+        // here we are going to see the maximum largest element subarray
+        // find maximum element and its index
           
-//             int n=nums.size();
-//         int maxelement=-1;
-//         int index=-1;
-//         for(int i=0;i<n;i++){
-//             if(nums[i]>maxelement){
-//                 maxelement=nums[i];  
-//                 index=i;
-//             }
-//         }
-       
-        
-//         int currentstreak=0,maxstreak=1,right=index;
-        
-//         while(right<n){
-//             if(nums[right]==maxelement){
-//                 currentstreak++;
-//                 maxstreak=max(currentstreak,maxstreak); 
-//             }
-//             else{
-//                 currentstreak=0;
-//             }
-//             right++;
-//         }
-        
-   
-//     return maxstreak;
-//     }
+         
 
-//     // learning always do not think of sliding window for size calculation subaaray
-//     // if you can do it by single variable
-//     // agar multiple subarray ho toh single element se ho sakta hain
+        int n=nums.size(),maxelement=*max_element(nums.begin(),nums.end());
 
-// };
-
- int longestSubarray(vector<int>& nums) {
-       int mx=-1,n=size(nums);
-        for(auto& el : nums)
-            mx=max(el, mx);
-        
-        int i=0,j=0,res=1;
-        while(j<n){
-            if(nums[j]==mx){
-                res=max(res, j-i+1);
-                j++;
+        // beacause and decrese it
+     
+       int i=0,j=0;
+       int maxsize=1;
+       while(i<n){
+          if(nums[i]==maxelement){
+            j=i;
+            while(j<n and nums[j]==maxelement){
+                 maxsize=max(maxsize,(j-i+1));
+                 j++;
             }
-            else{
-                j++;
-                i=j;
-            }
-        }
-        
-        return res;
+            i=j;
+          }
+          else{
+            i++;
+          }
+
+       }
+    
+    return maxsize;
     }
-};  
-
+};
