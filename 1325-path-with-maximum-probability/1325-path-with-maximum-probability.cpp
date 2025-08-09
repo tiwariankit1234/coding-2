5da61a0f43ed62040165416d3976a3fe21f1f129
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-08-09 18:43:28 +0530

    Time: 59 ms (37.81%), Space: 70.9 MB (57.04%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,34 +1,37 @@
+#define pii pair<double,int>
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        using pii = pair<double, int>;
-        vector<vector<pii>> adj(n);
-        vector<double> dist(n, 0.0);
-
-        int k = edges.size();
-        for (int i = 0; i < k; i++) {
-            adj[edges[i][0]].push_back({succProb[i], edges[i][1]});
-            adj[edges[i][1]].push_back({succProb[i], edges[i][0]});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<vector<pii>>adj(n);
+        vector<double>dist(n,0.0);
+        dist[start]=1.0;
+        int k=edges.size();
+        for(int i=0;i<k;i++){
+            adj[edges[i][0]].push_back({succProb[i],edges[i][1]});
+            adj[edges[i][1]].push_back({succProb[i],edges[i][0]});
         }
-
-        priority_queue<pii> pq;
-        dist[start_node] = 1.0;
-        pq.push({1.0, start_node});
-
-        while (!pq.empty()) {
-            auto [prob, node] = pq.top();
+        priority_queue<pii>pq;
+        pq.push({1.0,start});
+       
+        while(pq.size()){
+            auto [cost,node]=pq.top();
+            
             pq.pop();
-
-            if (prob < dist[node]) continue;
-
-            for (auto [edgeProb, adjnode] : adj[node]) {
-                if (dist[node] * edgeProb > dist[adjnode]) {
-                    dist[adjnode] = dist[node] * edgeProb;
-                    pq.push({dist[adjnode], adjnode});
+            for(auto it:adj[node]){
+                int adjnode=it.second;
+                double nextcost=it.first;
+                // cout<<node<<" "<<adjnode<<" "<<dist[node]<<" "<<dist[adjnode]<<" "<<dist[node]*nextcost<<" "<<dist[adjnode]<<endl;
+                if( (dist[node]*nextcost)>dist[adjnode]){
+                    dist[adjnode]=dist[node]*nextcost;
+                    pq.push({dist[adjnode],adjnode});
                 }
             }
         }
-
-        return dist[end_node];
+        // for(auto it:dist){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
+        return dist[end];
+        
     }
 };
