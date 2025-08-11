22c5307c0e3133f10d6800f199a536ed80b5a92d
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-08-11 20:03:04 +0530

    Time: 0 ms (100%), Space: 10.9 MB (89.31%) - LeetHub

---
+++ 3033-apply-operations-to-make-two-strings-equal/3033-apply-operations-to-make-two-strings-equal.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int dp[501];
+    int diffIdx[501];
+    int sz;
+
+    int dfs(int i, int x) {
+        if (i + 1 >= sz)
+            return (i % 2) * x;
+
+        if (dp[i] == 0) {
+            dp[i] = min(
+                dfs(i + 1, x) + x,  // Directly fix one mismatch
+                dfs(i + 2, x) + (diffIdx[i + 1] - diffIdx[i]) * 2 // Pair fix
+            );
+        }
+        return dp[i];
+    }
+
+    int minOperations(string s1, string s2, int x) {
+        sz = 0;
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = 0; i < (int)s1.size(); ++i) {
+            if (s1[i] != s2[i])
+                diffIdx[sz++] = i;
+        }
+
+        if (sz % 2) return -1; // Odd number of mismatches is impossible
+        return dfs(0, x) / 2;
+    }
+};
