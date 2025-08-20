55d389deb26e32f8e9dd2e574bbec4b63da82ce8
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-08-20 16:59:02 +0530

    Time: 12 ms (22.71%), Space: 32.9 MB (6.35%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>>& matrix) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-
-        vector<vector<int>>dp(rows+1,vector<int>(cols+1,0));
-
-        for(int j=0;j<cols;j++)
-        {
-            if(matrix[0][j]==1)
-            dp[0][j]=1;
+    int countSquares(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size(),count=0;
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int j=0;j<n;j++){
+            if(grid[0][j]==1){
+                dp[0][j]=1;
+               
+            }
         }
-
-        for(int i=0;i<rows;i++)
-        {
-            if(matrix[i][0]==1)
-            dp[i][0]=1;
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1){
+                dp[i][0]=1;
+                
+            }
         }
-
-        for(int i=1;i<rows;i++){
-            for(int j=1;j<cols;j++){
-                if(matrix[i][j]==1){
-                    dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(grid[i][j]==1){
+                    dp[i][j]=min({(int)dp[i-1][j-1],(int)dp[i][j-1],(int)dp[i-1][j]})+1;;
+                
                 }
             }
         }
-
-      int count=0;
-      for(auto it:dp){
-        for(auto k:it){
-            count+=k;
+        for(auto it:dp){
+            for(auto k:it){
+               count+=k;
+               
+            }
+            
         }
-      }
-      return count;
+        return count;
+
     }
 };
