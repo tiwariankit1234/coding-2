dfee8c63b674e371f20dd4d155cd8fdd33d7ecb8
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-09-03 23:53:30 +0530

    Time: 3 ms (34.99%), Space: 63 MB (12.4%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,7 +5,7 @@ public:
         int i=0,j=n-1;
         int water=0;
 
-        while(i<=j){
+        while(i<j){
             if(nums[i]<=nums[j]){
                 water=max(water,min(nums[i],nums[j])*(j-i));
                 i++;
