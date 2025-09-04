646976efb185c30f2a04a679aaa744becaaac11c
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-09-04 14:52:41 +0530

    Time: 1678 ms (5.02%), Space: 89 MB (18.82%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // set creation 
-        // then see x-1 not present 
-        // store in some vector 
-        // calculate size 
-        // calculate maxsize
         int n=nums.size();
-        
+        unordered_map<int,int>mp;
+        int maxcount=0;
+        for(int i=0;i<n;i++){
+            if(mp.find(nums[i])!=mp.end()){
+                continue;
+            }
+            else{
+                int num=nums[i];
+            int left=mp.count(num-1)?mp[num-1]:0;
+            int right=mp.count(num+1)?mp[num+1]:0;
+            int length=left+right+1;
+            maxcount=max(maxcount,length);
+            mp[num]=length;
+            mp[num-left]=length;
+            mp[num+right]=length;
+            cout<<left<<" "<<right<<endl;
+            // cout<<num<<" "<<mp[num-left]<<" "<<mp[num+right]<<" "<<length<<endl;
 
-        unordered_set<int>s(nums.begin(),nums.end());
-        if(s.size()==0)return 0;
-        if(s.size()==1)return 1;
-        vector<int>selectednumber;
-        for(int i:s){
-            if(!(s.contains(i-1))){
-                selectednumber.push_back(i);
             }
         }
-        // see for each number is present in s
-               int maxcount=1;
-        for(auto i:selectednumber){
-            int num=i;
-            int count=1;
-            while((s.contains(num+1))){
-                count++;
-                maxcount=max(count,maxcount);
-                num=num+1;
-            }
-        }
-       return maxcount;
-
+        cout<<mp[1]<<" "<<mp[4]<<endl;
+        return maxcount;
     }
 };
