20ca9714a87cfd4227b1ea41f0696bb55d040a79
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-09-11 19:37:51 +0530

    Time: 13 ms (88.79%), Space: 127.9 MB (94.91%) - LeetHub

--- 4000-count-bowl-subarrays/4000-count-bowl-subarrays.cpp
+++ 4000-count-bowl-subarrays/4000-count-bowl-subarrays.cpp
@@ -1,61 +1,80 @@
-#define ll long long 
+// #define ll long long 
+// class Solution {
+// public:
+// void prevgreater(vector<int>&nums,vector<int>&prev){
+//     int n=nums.size();
+//     stack<int>st;
+//     st.push(0);
+//     for(int i=1;i<n;i++){
+//         int num=nums[i];
+//         while(!st.empty() and num>nums[st.top()]){
+//             st.pop();
+//         }
+//         if(st.empty())prev[i]=-1;
+//         else{
+//           prev[i]=st.top();
+//         }
+//         st.push(i);
+//     }
+//     return ;
+// }
+
+// void nextgreater(vector<int>&nums,vector<int>&next){
+//     int n=nums.size();
+//     stack<int>st;
+//     st.push(n-1);
+//     for(int i=n-2;i>=0;i--){
+//         int num=nums[i];
+//         while(!st.empty() and num>nums[st.top()]){
+//             st.pop();
+//         }
+//         if(!st.empty()){
+//             next[i]=st.top();
+//         }
+//         st.push(i);
+//     }
+//     return ;
+// }
+//     long long bowlSubarrays(vector<int>& nums) {
+//         int n=nums.size();
+//         ll count=0;
+//         // prev greater element
+//         vector<int>prev(n,-1),next(n,-1);
+//           prevgreater(nums,prev);
+//           nextgreater(nums,next);
+//         //   for(auto it:prev){
+//         //     cout<<it<<" ";
+//         //   }
+//         //   cout<<endl;
+//         //   for(auto it:next){
+//         //     cout<<it<<" ";
+//         //   }
+//           cout<<endl;
+//         for(int i=1;i<(n-1);i++){
+//             if(prev[i]!=-1 and next[i]!=-1){
+//                 count++;
+//             }
+//         }
+//         return count;
+
+//     }
+// };
+
 class Solution {
 public:
-void prevgreater(vector<int>&nums,vector<int>&prev){
-    int n=nums.size();
-    stack<int>st;
-    st.push(0);
-    for(int i=1;i<n;i++){
-        int num=nums[i];
-        while(!st.empty() and num>nums[st.top()]){
-            st.pop();
-        }
-        if(st.empty())prev[i]=-1;
-        else{
-          prev[i]=st.top();
-        }
-        st.push(i);
-    }
-    return ;
-}
-
-void nextgreater(vector<int>&nums,vector<int>&next){
-    int n=nums.size();
-    stack<int>st;
-    st.push(n-1);
-    for(int i=n-2;i>=0;i--){
-        int num=nums[i];
-        while(!st.empty() and num>nums[st.top()]){
-            st.pop();
-        }
-        if(!st.empty()){
-            next[i]=st.top();
-        }
-        st.push(i);
-    }
-    return ;
-}
     long long bowlSubarrays(vector<int>& nums) {
-        int n=nums.size();
-        ll count=0;
-        // prev greater element
-        vector<int>prev(n,-1),next(n,-1);
-          prevgreater(nums,prev);
-          nextgreater(nums,next);
-        //   for(auto it:prev){
-        //     cout<<it<<" ";
-        //   }
-        //   cout<<endl;
-        //   for(auto it:next){
-        //     cout<<it<<" ";
-        //   }
-          cout<<endl;
-        for(int i=1;i<(n-1);i++){
-            if(prev[i]!=-1 and next[i]!=-1){
-                count++;
+        long long ans = 0;
+        stack<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!s.empty() && nums[s.top()] < nums[i]) {
+                s.pop();
+                if (!s.empty()) {
+                    ans++;
+                }
             }
+            s.push(i);
         }
-        return count;
 
+        return ans;
     }
 };
