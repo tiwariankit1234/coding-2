d50b8896589acf925e74f00e9575072e3aa79514
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-09-27 20:01:47 +0530

    Time: 17 ms (26.52%), Space: 11.7 MB (25.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
+bool check(unordered_map<char,int>&mp1,unordered_map<char,int>&mp2){
+    if((int)mp1.size()<(int)mp2.size())return false;
+    for(auto it:mp2){
+        char ch=it.first;
+        int freq=it.second;
+        if(mp1[ch]-mp2[ch]<0 )return false;
+    }
+    return true;
+}
     string minWindow(string s, string t) {
-        map<int,int>mp;
-        int n=s.size();
-        for(char c:t){
-            mp[c]++;
+        unordered_map<char,int>mp2,mp1;
+        int charactercnt=0;
+        for(auto it:t){
+            mp2[it]++;
+            charactercnt++;
         }
-        int k=0;
-        for(auto it:mp)
-        k+=it.second;
-
-        int stidx=-1;
-      
-        int charcnt=0,l=0,r=0,length=INT_MAX;
+         int lefty=-1,righty=-1,n=s.size(),ans=INT_MAX,l=0,r=0,formed=0;
         while(r<n){
-           mp[s[r]]--;
-           if(mp[s[r]]>=0){
-            charcnt++;
-           }
-           while(charcnt==k){
-             mp[s[l]]++;
-             if(mp[s[l]]>0){
-                charcnt--;
-                if((r-l+1)<length){
-                    length=r-l+1;
-                    stidx=l;
+            mp1[s[r]]++;
+            if(mp2.find(s[r])!=mp2.end() and mp1[s[r]]<=mp2[s[r]]){
+                formed++;
+            }
+            while(l<=r and formed==charactercnt){
+                if((r-l+1)<ans){
+                    lefty=l;
+                    righty=r;
                 }
-             }
-             l++;
-           }
-           r++;
-        }
-        if(stidx==-1)return "";
-        return s.substr(stidx,length);
+                ans=min(ans,r-l+1);
+                if(mp2.find(s[l])!=mp2.end() and mp1[s[l]]<=mp2[s[l]])formed--;
+                mp1[s[l]]--;
+                if(mp1[s[l]]==0)mp1.erase(s[l]);
+                l++;
+            }
+            r++;
+         }
+         if(lefty==-1 and righty==-1)return "";
+          return s.substr(lefty,righty-lefty+1);
+
     }
 };
