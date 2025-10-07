87b8556c9123af6d436487ec94b41fcf7146d3de
tiwariankit1234 <ankittiwari002003@gmail.com>
2025-10-07 23:48:09 +0530

    Time: 4 ms (20.95%), Space: 16.2 MB (48.78%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,51 +1,24 @@
-#include <vector>
-#include <map>
-#include <set>
-#include <queue>
-using namespace std;
-
-// Definition for a binary tree node.
-// struct TreeNode {
-//     int val;
-//     TreeNode *left;
-//     TreeNode *right;
-//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
-
 class Solution {
-public:
-    void inorder(TreeNode* root, int x, int y, map<int, map<int, multiset<int>>>& nodes) {
-        if (!root) return;
-        
-        // Visit left
-        inorder(root->left, x - 1, y + 1, nodes);
-        
-        // Process current node
-        nodes[x][y].insert(root->val);
-        
-        // Visit right
-        inorder(root->right, x + 1, y + 1, nodes);
-    }
-
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;  // x -> y -> multiset of node values
-        
-        // Perform in-order traversal to populate the map
-        inorder(root, 0, 0, nodes);
-
-        vector<vector<int>> ans;
-        for (auto& p : nodes) {
-            vector<int> col;
-            for (auto& q : p.second) {
-                for (auto val : q.second) {
-                    col.push_back(val);
-                }
+    public:
+        vector<vector<int>> verticalTraversal(TreeNode* root) 
+        {
+            map<int, multiset<pair<int, int>>> mp; // [x][y, val]
+            traverse(root, 0, 0, mp);
+            vector<vector<int>> res;
+            for(auto& [x, st] : mp)
+            {
+                res.push_back({});
+                for(auto& [y, val] : st) res.back().push_back(val);
             }
-            ans.push_back(col);
+            return res;
         }
-
-        return ans;
-    }
+        
+    protected:
+        void traverse(TreeNode* node, int x, int y,  map<int, multiset<pair<int, int>>>& mp)
+        {
+            if(!node) return;
+            mp[x].insert({y, node->val});
+            traverse(node->left, x-1, y+1, mp);
+            traverse(node->right, x+1, y+1, mp);
+        }
 };
