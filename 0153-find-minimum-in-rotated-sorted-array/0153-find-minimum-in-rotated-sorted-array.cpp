25a83ff5e903e904cce473861e9d15c05cec08bb
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-10-17 23:52:21 +0530

    Time: 0 ms (100%), Space: 14.1 MB (78.78%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,3 +1,9 @@
+//Hey @Prathmesh Nikam! It's simple only man: If you are moving your pointers by mid + 1 or mid - 1 idx then you must use left <= right
+
+// Otherwise, you must use left < right if you are moving your pointers by mid idx only.
+
+// Hope that helps! try it out and see the magic :)
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
