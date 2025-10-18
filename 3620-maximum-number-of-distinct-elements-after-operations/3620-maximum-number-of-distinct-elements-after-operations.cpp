3052bd2d2a3c8a49072c8653b174a929a47bf3e2
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-10-19 00:26:41 +0530

    Time: 455 ms (5.36%), Space: 170.1 MB (5.68%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,26 +1,28 @@
+
 class Solution {
 public:
+      set<int>st;
     int maxDistinctElements(vector<int>& nums, int k) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int last=nums[0]-k;
+        int n = nums.size();
+        map<int,int>mp;
+        for(auto it : nums)mp[it]++;
+        if(k == 0)return mp.size();
+        vector<vector<int>>intr;
+       int ans =0;
         
-        int count=0;
-        for(int i=0;i<n;i++){
-            int valend=nums[i]+k;
-            int  valstart=nums[i]-k;
-             if(last<valstart){
-                last=max(valstart,last);
-                last++;
-                count++;
-             }
+        sort(nums.begin(), nums.end());
+        int f = nums[0]-k;
+        for(auto it : nums){
+            int str = it-k, end = it + k;
+            f = max(str, f);
             
-           else if(last>=valstart and last<=valend){
-                count++;
-                last++;
+            if( f<=end and st.count(f)==0){
+               st.insert(f);
+                f++;ans++;
             }
-        
+            
         }
-        return count;
+
+return ans;
     }
 };
