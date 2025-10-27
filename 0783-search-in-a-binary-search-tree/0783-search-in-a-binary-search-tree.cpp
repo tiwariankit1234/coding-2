6c735ca36dddb28616d88f157171259896328836
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-10-27 13:44:35 +0530

    Time: 0 ms (100%), Space: 35.5 MB (27.54%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -9,50 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     TreeNode* searchBST(TreeNode* root, int val) {
-//         if(root==NULL)return NULL;
-      
-       
-//      TreeNode* left=searchBST(root->left,val);
-//         if(root->val==val)return root;
-//         if(left)return left;
-       
-//       TreeNode* right=searchBST(root->right,val);
-//        if(right)
-//        return right;
-     
-//      return NULL;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     TreeNode* searchBST(TreeNode* root, int val) {
-//         if(root==NULL || root->val==val)return root;
-      
-//        return root->val>val? searchBST(root->left,val):searchBST(root->right,val);
-
-//     }
-    
-// };
-
 class Solution {
-    public:
-    TreeNode* searchBST(TreeNode* root,int val){
-
-        while(root!=NULL and root->val!=val){
-           if(val<root->val){
-            root=root->left;
-           }
-           else{
-            root=root->right;
-           }
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* left;
+        TreeNode* right;
+        while(root){
+            if(root->val==val){
+                return root;
+            }
+            else if(root->val>val){
+              root=root->left;
+            }
+            else{
+              root=root->right;
+            }
         }
-    
-    return root;
-}
+        return NULL;
+    }
 };
-
