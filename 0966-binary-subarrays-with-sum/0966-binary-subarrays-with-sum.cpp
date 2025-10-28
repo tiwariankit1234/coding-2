9e835fbc73d7f3af71ef6657f614175ec865ec31
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-10-28 20:10:32 +0530

    Time: 25 ms (14.1%), Space: 40.3 MB (7.87%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -4,11 +4,9 @@ public:
         int n=nums.size();
         unordered_map<int,int>mp;
         int sum=0,count=0;
+        mp[0]=1;
         for(int i=0;i<n;i++){
             sum+=nums[i];
-            if(sum==goal){
-                count++;
-            }
             count+=mp[sum-goal];
             mp[sum]++;
         }
