a21d14b7d8dcf0bdf507039d0896ed6ec4a115fe
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-10-29 12:29:59 +0530

    Time: 0 ms (100%), Space: 8.1 MB (74.79%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int cmin=0,cmax=0;
+        for(char c:s){
+            if(c=='('){
+                cmax++;
+                cmin++;
+            }
+            else if(c==')'){
+                cmax--;
+                cmin--;
+            }
+            else if(c=='*'){
+                cmax++;
+                cmin--;
+            }
+            if(cmax<0)return false;
+            cmin=max(cmin,0);
+        }
+        return cmin==0;
+    }
+};
