acf8900adda69bef6256d67aa6f009df25f5ac1f
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-10-29 22:59:27 +0530

    Time: 3 ms (89.67%), Space: 23.7 MB (81.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int n=arr.size();
         vector<vector<int>>ans;
-
         sort(arr.begin(),arr.end());
-
-       int prevstart=arr[0][0];
-       int prevend=arr[0][1];
-         cout<<"run befor loop"<<endl;
-         ans.push_back({prevstart,prevend});
-
-        for(int i=1;i<n;i++){
-            int start=arr[i][0];
-            int end=arr[i][1];
-            cout<<"isindexpar chl raha"<<" "<<i<<endl;
-            // check merged condition
-            if(start<=prevend){
-              
-             
-              ans.back()[1]=max(prevend,end);
-              prevend=max(end,prevend);
+        ans.push_back(arr[0]);
+        for(int i=1;i<(arr.size());i++){
+            if(arr[i][0]<=ans.back()[1]){
+                ans.back()[0]=min(ans.back()[0],arr[i][0]);
+                ans.back()[1]=max(ans.back()[1],arr[i][1]);
             }
             else{
-                ans.push_back({start,end});
-                prevstart=start;
-                prevend=end;
+                ans.push_back(arr[i]);
             }
-   
         }
         return ans;
     }
