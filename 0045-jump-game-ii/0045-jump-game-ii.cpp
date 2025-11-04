432e42816be68ce8a6ff4af9a704a98a49e871b0
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-04 21:54:28 +0530

    Time: 0 ms (100%), Space: 20.4 MB (62.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,32 +1,16 @@
-
 class Solution {
 public:
-int dp[10005];
-int f(int i,vector<int>& nums,int n){
-    // cout<<i<<endl;
-    
-    if(i==n-1){
-        return 0;
-    }
-    if(nums[i]==0)return 90000;
-  
-   if(dp[i]!=-1)return dp[i];
-   
-    
-   
-    int ans=90000;
-    int steps=nums[i];
-    for(int j=steps;j>=1;j--){
-        if((i+j)<n){
-        ans=min(ans,1+f(i+j,nums,n));
-        }
-    }
-    return dp[i]=ans;
-}
     int jump(vector<int>& nums) {
         int n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        // cout<<"5"<<endl;
-        return f(0,nums,n);
+        int l=0,r=0,farthest=0,jumps=0;
+        while(r<(n-1)){
+            for(int index=l;index<=r;index++){
+                farthest=max(farthest,nums[index]+index);
+            }
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+         return jumps;
     }
 };
