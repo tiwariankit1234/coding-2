cd6a146ccf17c7b079d1de658e7bc4cf00ed3402
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-15 19:20:23 +0530

    Time: 35 ms (87.39%), Space: 19 MB (59.73%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,50 +1,69 @@
 class Solution {
 public:
-bool possible(string &s,string &t){
-    int m=s.size(),n=t.size();
-    int i=0,j=0;
-    bool flag=false;
-    if(s.size()!=t.size()+1)return false;
-    while(i<m){
-        if(s[i]==t[j]){
-            i++;
-            j++;
+    bool possible(string &longer, string &shorter) {
+        int m = longer.size(), n = shorter.size();
+        if (m != n + 1) return false;
+
+        int i = 0, j = 0;
+        bool removed = false;
+
+        while (i < m) {
+            if (j < n && longer[i] == shorter[j]) {
+                i++; 
+                j++;
+            }
+            else {
+                if (removed) return false;
+                removed = true;
+                i++;
+            }
         }
-        else if(!flag and s[i]!=t[j]){
-            flag=true;
-           i++;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
-    return true;
-}
+
     int longestStrChain(vector<string>& words) {
-        int n=words.size(),ans=1;
-        sort(words.begin(),words.end(),[](string &a,string &b){
-            return (a.size()<b.size());
+        int n = words.size();
+
+        // sort by length
+        sort(words.begin(), words.end(), [](string &a, string &b) {
+            return a.size() < b.size();
         });
-         for(auto it:words){
-            cout<<it<<" ";
-        }
-        vector<int>dp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(possible(words[i],words[j])){
-                    cout<<i<<" "<<j<<" "<<words[i]<<" "<<words[j]<<endl;
-                    if(dp[j]+1>dp[i]){
-                        dp[i]=dp[j]+1;
-                        ans=max(ans,dp[i]);
+
+        vector<int> dp(n, 1);
+        vector<int> parent(n);
+
+        int bestLen = 1, lastIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;  
+            for (int j = 0; j < i; j++) {
+                if (possible(words[i], words[j])) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
                     }
                 }
             }
+
+            if (dp[i] > bestLen) {
+                bestLen = dp[i];
+                lastIndex = i;
+            }
         }
-        // for(auto it:dp){
-        //     cout<<it<<" ";
-        // }
-        // cout<<endl;
-        
-        return ans;
+
+        // OPTIONAL: print the chain
+        vector<string> chain;
+        chain.push_back(words[lastIndex]);
+        while (parent[lastIndex] != lastIndex) {
+            lastIndex = parent[lastIndex];
+            chain.push_back(words[lastIndex]);
+        }
+
+        reverse(chain.begin(), chain.end());
+
+        for (auto &w : chain) cout << w << " ";
+        cout << endl;
+
+        return bestLen;
     }
 };
