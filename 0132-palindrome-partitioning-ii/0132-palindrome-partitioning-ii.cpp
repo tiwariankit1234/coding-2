1cf720a3247e87067e157f4e5375900163fbedc4
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-17 11:11:07 +0530

    Time: 42 ms (75.4%), Space: 13.3 MB (24.43%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,57 +1,36 @@
-    class Solution{
-
-        public:
-    
-    
-    
-    bool isPalindrome (string & s, int i, int j){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
-            i++; j--;
-        }
-        return true;
-    }
-    
-
-    int solve (string & s, int i, int j, vector<int> & dp){    
-        
-        if(i>=j or isPalindrome(s, i, j)) return 0;
-        
-        if(dp[i]!=-1) return dp[i];
-        
-        int ans = INT_MAX;
-        
-        for(int k=i; k<j; k++){
-            
-            /* 
-                Instead of writing below standard line
-                We will recurse for only right part
-                Only when left part turns out to be palindrome
-				
-				Reason : If left substring becomes palindrome then there is no need to partition it further 
-				(which in turn reduces the number of recursive calls)
-                
-                int temp =  solve (s, i, k, dp, palindrome) + solve (s, k+1, j, dp, palindrome) + 1;
-                
-            */
-            
-            if(isPalindrome(s, i, k)){                         
-                int temp = solve (s, k+1, j, dp) + 1;
-                ans = min (ans, temp);
+class Solution {
+public:
+bool ispal[2002][2002];
+int dp[2002];
+int solve(int idx,string &s){
+    int n=s.size();
+    if(idx==n)return 0;
+    if(dp[idx]!=-1)return dp[idx];
+    int ans=INT_MAX;
+    for(int j=idx;j<n;j++){
+        if(ispal[idx][j]){
+            if(j==n-1){
+                ans=0;
+            }
+            else{
+                ans=min(ans,1+solve(j+1,s));
             }
         }
-        
-        return dp[i] = ans;
     }
-    
+    return dp[idx]=ans;
+}
     int minCut(string s) {
-        int n = s.length();
-        
-        /* I've used 1D DP here becoz only one parameter is changing i.e. `i` */
-        
-        vector<int> dp (n+1, -1);   
-            
-        return solve (s, 0, n-1, dp);
+        int n=s.size();
+        memset(dp,-1,sizeof(dp));
+        memset(ispal,false,sizeof(ispal));
+        for(int len=1;len<=n;len++){
+            for(int i=0;(i+len-1)<n;i++){
+                int j=i+len-1;
+                if(s[i]==s[j] and (len<=2 || ispal[i+1][j-1])){
+                    ispal[i][j]=true;
+                }
+            }
+        }
+        return solve(0,s);
     }
-
-    };
+};
