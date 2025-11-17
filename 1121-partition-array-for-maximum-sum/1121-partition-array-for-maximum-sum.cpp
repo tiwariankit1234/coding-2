0731ae0a8529661fdb96c8b52640ce85ff65ac3f
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-17 10:28:04 +0530

    Time: 11 ms (24.72%), Space: 12 MB (93.77%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
 int dp[502];
-// try all partions 
-// no need of end 
-// always the variable will change the state 
-// there will be no fixed loop occur in loop
-// like start+k
-int f(int start,int end,int k,vector<int>& arr){
-   int n=arr.size();
-   if(start==n)return 0;
-   if(dp[start]!=-1)return dp[start];
-   int maxsum=0;
-   int maxelement=0;
-   for(int i=start;i<min(n,start+k);i++){
-     maxelement=max(arr[i],maxelement);
-     maxsum=max(maxsum,maxelement*(i-start+1)+f(i+1,end,k,arr));
-   }
-   return dp[start]=maxsum;
+int f(int i,vector<int>&arr,int m){
+  
+    int n=arr.size();
+    if(i==n)return 0;
+    if(dp[i]!=-1)return dp[i];
+    int ans=INT_MIN;
+    int maxi=INT_MIN;
+    int temp=0;
+    for(int k=i;k<=min((i+m-1),n-1);k++){
+        
+        maxi=max(maxi,arr[k]);
+        temp=maxi*(k-i+1)+f(k+1,arr,m);
+        ans=max(temp,ans);
+    }
+    return dp[i]=ans;
 }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
         memset(dp,-1,sizeof(dp));
-        return f(0,n-1,k,arr);
+        return f(0,arr,k);
     }
 };
