80183496e66dbf4d1af951ea9ad2c89833b82781
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-18 22:01:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.25%) - LeetHub

---
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(!head->next)return head;
+        
+         ListNode *slow = head; 
+    
+     // Initialize the fast pointer to the head.
+    ListNode *fast = head; 
+
+    // Traverse the linked list using the
+    // Tortoise and Hare algorithm.
+    while (fast != NULL && fast->next != NULL) {
+        // Move slow one step.
+        slow = slow->next; 
+         // Move fast two steps.
+        fast = fast->next->next; 
+    }
+    
+    
+     // Return the slow pointer,
+     // which is now at the middle node.
+    return slow; 
+    }
+};
