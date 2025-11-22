e8fa034ea81a2eef23bae752a6bbca57c86759b3
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-22 18:39:04 +0530

    Time: 0 ms (100%), Space: 16.3 MB (93.65%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,54 +1,55 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
-int f(ListNode* head){
-    ListNode* temp=head;
-    int count=0;
-    while(temp){
-        count++;
-        temp=temp->next;
-    }
-    return count;
-}
-    ListNode* rotateRight(ListNode* head, int k) {
-      int size=f(head),count=0;
-      if(k==0)return head;
-      if(!head)return head;
-      if(!(head->next))return head;
-      if(size)
-      k=k%size;
-      if(k==0)return head;
-    //   cout<<size<<endl;
-      ListNode *temp=head;
-      ListNode* prev=NULL;
-      ListNode* curr=head;
 
-      while(count<(size-k)){
-        prev=curr;
-         curr=curr->next;
-        count++;
-      }
-     cout<<size-k<<" "<<count<<endl;
-      
-       if(prev)prev->next=NULL;
-     ListNode *newhead=curr;
-     prev=curr;
-      while(curr and curr->next){
-        curr=curr->next;
-       prev->next=curr;
-       prev=curr;
-      }
-     
-    if(curr) curr->next=head;
-     return newhead;
+ void reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        while (curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return;
+    }
+    int sizy(ListNode* curr){
+        int size=0;
+        while(curr){
+           size+=1;
+           curr=curr->next;
+        }
+        return size;
+    }
+    ListNode* f(ListNode* temp,int k){
+        while(k>0 and temp){
+            k--;
+            temp=temp->next;
+        }
+        return temp;
+    }
+
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head)return NULL;
+        if(!head->next)return head;
+        int size=sizy(head);
+       
+        k=k%size;
+         if(k==0)return head;
+
+        ListNode* temp=head;
+        ListNode* kthnode=NULL;
+         kthnode=f(temp,size-(k+1));
+         ListNode* newhead=kthnode->next;
+         ListNode* nextnode=kthnode->next;
+        while(temp->next){
+            temp=temp->next;
+        }
+        kthnode->next=NULL;
+        temp->next=head;
+        return newhead;
+
     }
 };
