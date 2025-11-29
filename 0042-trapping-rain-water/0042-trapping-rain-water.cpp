18db95527368c805ca857273b9f160da5c23e56c
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-11-29 23:11:02 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -5,15 +5,19 @@ public:
         int left=0,right=n-1;
         int leftmax=0,rightmax=0;
         int water=0;
-        while(left<=right){
+        while(left<right){
             if(height[left]<height[right]){
-                leftmax=max(leftmax,height[left]);
+                 leftmax=max(leftmax,height[left]);
                 water+=leftmax-height[left];
+               
+                
                 left++;
             }
             else{
-                rightmax=max(rightmax,height[right]);
-                water+=rightmax-height[right];
+                 rightmax=max(rightmax,height[right]);
+                  water+=rightmax-height[right];
+               
+              
                 right--;
             }
         }
