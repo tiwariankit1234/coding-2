ef629322b9212727c6fe71fed2da96f5b34feea4
Ankit Tiwari <ankittiwari002003@gmail.com>
2025-12-09 04:16:31 +0530

    Time: 2 ms (88.46%), Space: 54.1 MB (76.92%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+    let res=0;
+        for(let a=1;a<=n;a++){
+            for(let b=1;b<=n;b++){
+                let c=Math.floor(Math.sqrt(a*a+b*b));
+                if(c<=n && c*c==(a*a+b*b))res++;
+            }
+        }
+        return res;
+};
