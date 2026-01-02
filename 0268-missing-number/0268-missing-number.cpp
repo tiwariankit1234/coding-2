95ba36dcd934d8c701af45c2a7b713e610870363
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-01-02 17:44:01 +0530

    Time: 0 ms (100%), Space: 21.7 MB (68.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size(),misiixor=0;
-        for(int i=1;i<=n;i++){
-           misiixor=misiixor^i;
+        int n = nums.size(), i = 0;
+        while (i < n) {
+            int correctidx = nums[i];
+            if (correctidx == n)
+                i++;
+            else if (nums[i] != nums[correctidx]) {
+                swap(nums[i], nums[correctidx]);
+            } else {
+                i++;
+            }
         }
-        for(int i=0;i<n;i++){
-           misiixor=misiixor^nums[i];
+        i = 0;
+
+        cout << endl;
+        for (i = 0; i < n; i++) {
+            if (i != nums[i])
+                return i;
         }
-        return misiixor;
+        return i;
     }
 };
