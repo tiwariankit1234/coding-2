f6b02ee45a23ccda48be9688935dd4884f382c2b
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-01-07 20:31:04 +0530

    Time: 0 ms (100%), Space: 18.3 MB (5.13%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        int maxproduct=INT_MIN,product=1;
-        for(int i=0;i<n;i++){
-            product=product*nums[i];
-            maxproduct=max(maxproduct,product);
-            if(product==0){
-                maxproduct=max(maxproduct,product);
-                product=1;
-            }
+        vector<int>prefix(n,0),suffix(n,0);
+        int maxproduct=INT_MIN;
+        prefix[0]=nums[0];
+        maxproduct=max(maxproduct,prefix[0]);
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]*nums[i];
+            if(prefix[i-1]==0)prefix[i]=1*nums[i];
+            else
+            prefix[i]=prefix[i-1]*nums[i];
+            maxproduct=max(maxproduct,prefix[i]);
         }
-         product=1;
-        for(int i=n-1;i>=0;i--){
-            product=product*nums[i];
-            maxproduct=max(product,maxproduct);
-            if(product==0){
-                maxproduct=max(maxproduct,product);
-                product=1;
+        suffix[n-1]=nums[n-1];
+        maxproduct=max(maxproduct,suffix[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(suffix[i+1]==0){
+            suffix[i]=1*nums[i];
             }
+            else{
+                suffix[i]=suffix[i+1]*nums[i];
+            }
+            maxproduct=max(maxproduct,suffix[i]);
+        }
+        for(int i=0;i<n;i++){
+            maxproduct=max(maxproduct,nums[i]);
         }
         return maxproduct;
+        
+        
+
     }
 };
