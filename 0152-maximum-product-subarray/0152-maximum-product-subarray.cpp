0f512b8e8c938f293f8fb0d9f3e7be58c1069703
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-01-07 22:09:40 +0530

    Time: 0 ms (100%), Space: 17.7 MB (85.21%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
-        vector<int>prefix(n,0),suffix(n,0);
-        int maxproduct=INT_MIN;
-        prefix[0]=nums[0];
-        maxproduct=max(maxproduct,prefix[0]);
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*nums[i];
-            if(prefix[i-1]==0)prefix[i]=1*nums[i];
-            else
-            prefix[i]=prefix[i-1]*nums[i];
-            maxproduct=max(maxproduct,prefix[i]);
-        }
-        suffix[n-1]=nums[n-1];
-        maxproduct=max(maxproduct,suffix[n-1]);
-        for(int i=n-2;i>=0;i--){
-            if(suffix[i+1]==0){
-            suffix[i]=1*nums[i];
+        int maxproduct=INT_MIN,prevproduct=1;
+        for(int i=0;i<n;i++){
+
+            if(prevproduct==0){
+            int curr=1*nums[i];
+            maxproduct=max(maxproduct,curr);
+            prevproduct=curr;
             }
             else{
-                suffix[i]=suffix[i+1]*nums[i];
+                int curr=prevproduct*nums[i];
+                maxproduct=max(maxproduct,curr);
+                prevproduct=curr;
             }
-            maxproduct=max(maxproduct,suffix[i]);
         }
-        // for(int i=0;i<n;i++){
-        //     maxproduct=max(maxproduct,nums[i]);
-        // }
-        return maxproduct;
-        
-        
-
+         prevproduct=nums[n-1];
+         maxproduct=max(maxproduct,prevproduct);
+         for(int i=n-2;i>=0;i--){
+            if(prevproduct==0){
+            int curr=1*nums[i];
+            maxproduct=max(maxproduct,curr);
+            prevproduct=curr;
+            }
+            else{
+                int curr=prevproduct*nums[i];
+                maxproduct=max(maxproduct,curr);
+                prevproduct=curr;
+            }
+         }
+         return maxproduct;
     }
 };
