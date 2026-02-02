7912c31bf865db1c45b037d0eb9dd64a542585eb
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-02-02 08:06:58 +0530

    Time: 15 ms (0%), Space: 8.6 MB (63.64%) - LeetHub

---
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string reverseByType(string s) {
+        int n = s.size();
+        int i = 0, j = n - 1;
+        cout<<s[i]<<" "<<s[j]<<endl;
+        while (i < j) {
+            while (j>=0 and !(s[j] >= 'a' and s[j] <= 'z')) {
+                j--;
+            }
+            while (i<n and !(s[i] >= 'a' and s[i] <= 'z') and i<n) {
+                i++;
+            }
+            if(i>=0 and j<n and i<j )
+            swap(s[i], s[j]);
+            // cout<<i<<" "<<j<<endl;
+            i++;
+            j--;
+        }
+        cout<<i<<" "<<j<<endl;
+        i = 0, j = n - 1;
+
+        while (i < j) {
+            while (j>=0 and (s[j] >= 'a' and s[j] <= 'z')) {
+                j--;
+            }
+            while (i<n and (s[i] >= 'a' and s[i] <= 'z') ) {
+                i++;
+            }
+            cout<<i<<" "<<j<<endl;
+            if(i>=0 and j<n and i<j and i<n and j>=0)
+            swap(s[i], s[j]);
+
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
