dd2474cabd33a76a665953c2d8822d8f7281566c
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-02-28 22:28:17 +0530

    Time: 1054 ms (13.42%), Space: 165.4 MB (11.41%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,26 @@
+#define ll long long
+class Solution {
+public:
+ll mod=1e9+7;
+int f(int i){
+    string k="";
+    while(i>0){
+        if(i%2)k.push_back('1');
+        else k.push_back('0');
+        i=i/2;
+    }
+    reverse(k.begin(),k.end());
+    return k.size();
+}
+    int concatenatedBinary(int n) {
+        ll ans=0;
+        string s="";
+        
+        ans=1;
+        for(int i=2;i<=n;i++){
+            int k=f(i);
+            ans = ((ans * ((1LL << k) % mod)) % mod + i) % mod;
+        }
+        return ans;
+    }
+};
