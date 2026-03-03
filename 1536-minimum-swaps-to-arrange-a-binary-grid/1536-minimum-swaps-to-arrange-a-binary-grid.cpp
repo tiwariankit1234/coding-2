a37b4eed2cc651a22d434f7d7412ce1975435bd8
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-03-04 00:17:00 +0530

    Time: 0 ms (100%), Space: 29.7 MB (83.75%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<int> pos(n, -1);
-        for (int i = 0; i < n; ++i) {
-            for (int j = n - 1; j >= 0; --j) {
-                if (grid[i][j] == 1) {
-                    pos[i] = j;
-                    break;
-                }
+        int m=grid.size(),n=grid.size();
+         vector<int>required,temp;
+         for(int i=0;i<m;i++){
+            required.push_back(n-1-i);
+         }
+         for(int i=0;i<m;i++){
+            int count=0;
+            for(int j=n-1;j>=0;j--){
+              if(grid[i][j]==1)break;
+              count++;
             }
-        }
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int k = -1;
-            for (int j = i; j < n; ++j) {
-                if (pos[j] <= i) {
-                    ans += j - i;
-                    k = j;
-                    break;
+            temp.push_back(count);
+         }
+         int swaps=0;
+         for(int i=0;i<n;i++){
+            bool flag=false;
+            if(temp[i]<required[i]){
+                int index=-1;
+                for(int j=i+1;j<(n);j++){
+                    if(temp[j]>=required[i]){
+                        index=j;
+                        break;
+                    }
                 }
-            }
-            if (~k) {
-                for (int j = k; j > i; --j) {
-                    swap(pos[j], pos[j - 1]);
+                     if(index!=-1){
+                    while(index>i){
+                        swap(temp[index],temp[index-1]);
+                        swaps++;
+                        index--;
+                    }
+
                 }
-            } else {
-                return -1;
+                
             }
-        }
-        return ans;
+         }
+         for(int i=0;i<n;i++){
+            if(temp[i]<required[i])return -1;
+         }
+
+         return swaps;
     }
 };
