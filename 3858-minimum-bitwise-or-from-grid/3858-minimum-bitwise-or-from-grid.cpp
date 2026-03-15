24279cdd256fba89a7fbab6b9f0fc3af34cadc86
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-03-15 16:01:38 +0530

    Time: 9 ms (65.18%), Space: 126.4 MB (46%) - LeetHub

--- 3858-minimum-bitwise-or-from-grid/3858-minimum-bitwise-or-from-grid.cpp
+++ 3858-minimum-bitwise-or-from-grid/3858-minimum-bitwise-or-from-grid.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int minimumOR(vector<vector<int>>& grid) {
-        
-        int ans = (1 << 17) - 1;   // allow all bits initially
-        
-        for(int bit = 16; bit >= 0; bit--){
-            
-            int candidate = ans ^ (1 << bit);   // try removing this bit
-            
-            bool possible = true;
-            
-            for(auto &row : grid){
-                bool ok = false;
+        int m = grid.size(), n = grid[0].size();
+        int ans = (1 << 17) - 1;
+        for (int l = 16;l >= 0; l--) {
+          ans = ans & ~(1 << l);
+
+            for (int i = 0; i < m; i++) {
+                int count = 0;
+                bool ok=false;
                 
-                for(int x : row){
-                    if((x | candidate) == candidate){
-                        ok = true;
+                for (int j = 0; j < n; j++) {
+                    int x = ans | grid[i][j];
+                    if (x == ans) {
+                        ok=true;
                         break;
                     }
                 }
-                
                 if(!ok){
-                    possible = false;
+                    ans=ans|(1<<l);
                     break;
                 }
             }
-            
-            if(possible){
-                ans = candidate;
-            }
         }
-        
         return ans;
     }
 };
