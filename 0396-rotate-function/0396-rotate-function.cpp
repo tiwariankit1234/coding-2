31e197efb2fb59ae0997ec559aed479acde6b75b
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-05-01 18:55:33 +0530

    Time: 0 ms (100%), Space: 99.8 MB (14.26%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return 0;
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
+        int maxsum=INT_MIN;
+        int previousum=0;
+        for(int i=0;i<n;i++){
+            previousum+=(i*nums[i]);
+        }
+        maxsum=max(maxsum,previousum);
+
+        for(int i=1;i<=(n-1);i++){
+        int x=previousum+totalsum-n*nums[n-1-(i-1)];
+       
+        maxsum=max(maxsum,x);
+        // cout<<i<<" "<<x<<" "<<previousum<<" "<<totalsum<<endl;
+        previousum=x;
+        }
+        return maxsum;
+    }
+};
