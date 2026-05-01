b73c9c7bdbe33c7fd0402c868f30c90802003135
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-05-01 14:04:20 +0530

    Time: 2066 ms (5.07%), Space: 132.6 MB (90.23%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -1,38 +1,44 @@
+  int dp[201][201][403];
+
 class Solution {
 public:
-    int maxPathScore(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<vector<int>>> dp(
-            m, vector<vector<int>>(n, vector<int>(k + 1, INT_MIN)));
-        dp[0][0][0] = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int c = 0; c <= k; c++) {
-                    if (dp[i][j][c] == INT_MIN) continue;
-                    if (i + 1 < m) {
-                        int val = grid[i + 1][j];
-                        int cost = (val == 0 ? 0 : 1);
-                        if (c + cost <= k) {
-                            dp[i + 1][j][c + cost] =
-                                max(dp[i + 1][j][c + cost], dp[i][j][c] + val);
-                        }
-                    }
-                    if (j + 1 < n) {
-                        int val = grid[i][j + 1];
-                        int cost = (val == 0 ? 0 : 1);
-                        if (c + cost <= k) {
-                            dp[i][j + 1][c + cost] =
-                                max(dp[i][j + 1][c + cost], dp[i][j][c] + val);
-                        }
-                    }
-                }
-            }
+    int m, n;
+    vector<vector<int>> grid;
+  
+
+    int f(int i, int j, int k) {
+        if (k < 0) return -1e7;
+        if (i >= m || j >= n) return -1e7;
+
+        if (i == m-1 && j == n-1) {
+            if (grid[i][j] != 0)
+                return dp[i][j][k] = (k >= 1) ? grid[i][j] : -1e7;
+            else
+                return dp[i][j][k] = grid[i][j]; // k>=0 always true
         }
-        int ans = INT_MIN;
-        for (int c = 0; c <= k; c++) {
-            ans = max(ans, dp[m - 1][n - 1][c]);
-        }
-        return ans < 0 ? -1 : ans;
+
+        if (dp[i][j][k] != -10000007) return dp[i][j][k];
+
+        int cost = 0;
+        if (grid[i][j] != 0)
+            cost = grid[i][j] + max(f(i+1, j, k-1), f(i, j+1, k-1));
+        else
+            cost = grid[i][j] + max(f(i+1, j, k), f(i, j+1, k));
+
+        return dp[i][j][k] = cost;
+    }
+
+    int maxPathScore(vector<vector<int>>& mat, int k) {
+        m = mat.size(), n = mat[0].size();
+        grid = mat;
+        
+        k=min(402,k);
+        for (int i = 0; i < 201; i++)
+            for (int j = 0; j < 201; j++)
+                for (int k = 0; k < 403; k++)
+                    dp[i][j][k] = -10000007;
+
+        int ans = f(0, 0, k);
+        return ans >= 0 ? ans : -1;
     }
 };
