abfe71393a6e7b32c243a1edb1a2c04570b5b63c
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-05-05 00:14:50 +0530

    Time: 0 ms (100%), Space: 10.3 MB (8.3%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=i;j<n;j++){
-                swap(grid[i][j],grid[j][i]);
-            }
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> result(n, vector<int>(m));
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[j][i] = matrix[i][j];
         }
-        for(int i=0;i<m;i++){
-            reverse(grid[i].begin(),grid[i].end());
-        }
-        return ;
+    }
+
+    return result;
+}
+
+
+    void rotate(vector<vector<int>>& matrix) {
+        matrix=transpose(matrix);
+       int i=0,j=matrix[0].size()-1;
+      for(int i=0;i<matrix.size();i++)reverse(matrix[i].begin(),matrix[i].end());
+      return ;
     }
 };
