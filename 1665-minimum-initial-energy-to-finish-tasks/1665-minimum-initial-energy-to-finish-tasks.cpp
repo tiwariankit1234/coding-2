b8501606e0650d722b6fa584f51bed984a3f9ffe
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-05-12 23:23:17 +0530

    Time: 61 ms (38.35%), Space: 97.3 MB (85.92%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+bool possible(int mid,vector<vector<int>>& tasks){
+    for(auto &temp:tasks){
+        int x=temp[0],y=temp[1];
+        if(mid>=y){
+         mid=mid-x;
+        }
+        else{
+            return false;
+        }
+    }
+    return mid>=0?true:false;
+}
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(),tasks.end(),[](vector<int>&a,vector<int>&b){
+            if(a[1]-a[0]==b[1]-b[0]){
+                return a[1]<b[1]; 
+            }
+            return a[1]-a[0]>(b[1]-b[0]);
+        });
+        int  low=1,high=INT_MAX,ans=INT_MAX;
+        while(low<=high){
+            int mid=(low+(high-mid)/2);
+            if(possible(mid,tasks)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
