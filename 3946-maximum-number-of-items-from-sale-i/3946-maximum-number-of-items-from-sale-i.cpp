44a9bfe84e4f0549ef3d83a488b889b9e27f859f
Ankit Tiwari <ankittiwari002003@gmail.com>
2026-05-31 23:18:09 +0530

    Time: 581 ms (7.48%), Space: 214 MB (5.37%) - LeetHub

--- 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
+++ 3946-maximum-number-of-items-from-sale-i/3946-maximum-number-of-items-from-sale-i.cpp
@@ -1,62 +1,109 @@
-constexpr int ITEMS = 1001;
-constexpr int BUDGET = 1501;
-constexpr int FLAG = 2;
+
 
 class Solution {
+
 public:
 
-    int f(int i, bool flag, int budget,
-          vector<pair<int,int>>& temp,
-          int (&dp)[ITEMS][BUDGET][FLAG]) {
 
-        if (i == temp.size()) return 0;
 
-        if (dp[i][budget][flag] != -1)
-            return dp[i][budget][flag];
+    int f(int i,bool flag,int budget,vector<pair<int,int>>&temp,vector<vector<array<int,2>>>&dp){
 
-        int count = 0;
+        if(i==temp.size())return 0;
 
-        if (!flag && budget >= temp[i].second) {
-            count = max(count,
-                        1 + temp[i].first +
-                        f(i, true, budget - temp[i].second,
-                          temp, dp));
+        if(budget<0)return 0;
+
+         if(dp[i][budget][flag]!=-1)return dp[i][budget][flag];
+
+        int count=0;
+
+        if(flag==false and budget-temp[i].second>=0){
+
+            // int k=budget,x=0;
+
+            // while(k-temp[i].second>=0){
+
+            //     x++;
+
+            //     k=k-temp[i].second;
+
+            //     count=max(count,x+f(i+1,k,temp));
+
+            // }
+
+             int k=budget;
+
+             count=max(count,1+temp[i].first+f(i,true,budget-temp[i].second,temp,dp));
+
+        } 
+
+        else if(flag==true and budget-temp[i].second>=0){
+
+            count=max(count,1+f(i,true,budget-temp[i].second,temp,dp));
+
         }
-        else if (flag && budget >= temp[i].second) {
-            count = max(count,
-                        1 + f(i, true,
-                              budget - temp[i].second,
-                              temp, dp));
-        }
 
-        count = max(count,
-                    f(i + 1, false, budget,
-                      temp, dp));
+        count=max(count,f(i+1,false,budget,temp,dp));
 
-        return dp[i][budget][flag] = count;
+        return dp[i][budget][flag]=count;
+
     }
 
     int maximumSaleItems(vector<vector<int>>& items, int budget) {
 
-        vector<pair<int,int>> temp;
+        vector<pair<int, int>> temp;
+        // budget1=budget+1;
+        // items1=items.size()+1;
+        // flag1=2;
 
-        static int dp[ITEMS][BUDGET][FLAG];
-        memset(dp, -1, sizeof(dp));
+       vector<vector<array<int,2>>>dp(items.size()+1,vector<array<int,2>>(budget+1,{-1,-1}));
+       
+
+        // for(int i=0;i<items.size();i++){
+
+        //     for(int j=0;j<=budget;j++){
+
+        //         for(int k=0;k<2;k++){
+
+        //             dp[i][j][k]=false;
+
+        //         }
+
+        //     }
+
+        // }
 
         for (int i = 0; i < items.size(); i++) {
-            int factor = items[i][0];
-            int price  = items[i][1];
 
-            int cnt = 0;
+            int factor = items[i][0], price = items[i][1];
+
+            int count = 0;
 
             for (int j = 0; j < items.size(); j++) {
-                if (i != j && items[j][0] % factor == 0)
-                    cnt++;
+
+                if (j != i) {
+
+                    int factorj = items[j][0], price = items[j][1];
+
+
+
+                    if (j != i and factorj % factor == 0) {
+
+                        count++;
+
+                    }
+
+                }
+
             }
 
-            temp.push_back({cnt, price});
+            temp.push_back({count , price});
+
         }
 
-        return f(0, false, budget, temp, dp);
+        int ans = f(0,false, budget, temp,dp);
+
+        return ans;
+
     }
+
 };
